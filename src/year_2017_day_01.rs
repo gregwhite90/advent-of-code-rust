3fa4924eda82bd646d4b83f07baadbf77246b806
Gregory White <gregwhite90@gmail.com>
2023-09-27 14:34:52 -0400

    test: add tests for 2017-01 part one

--- src/year_2017_day_01.rs
+++ src/year_2017_day_01.rs
@@ -2,7 +2,7 @@ pub mod year_2017_day_01 {
     pub use unicode_segmentation::UnicodeSegmentation;
     pub use itertools::Itertools;
     
-    pub fn sum_of_repeated_characters(text: &str) -> u32{
+    pub fn sum_of_repeated_characters(text: &str) -> u32 {
         let graphemes: Vec<u32> = text
             .graphemes(true)
             .map(|digit| digit.parse::<u32>().unwrap())
@@ -10,5 +10,18 @@ pub mod year_2017_day_01 {
         graphemes.iter()
             .circular_tuple_windows()
             .fold(0, |acc, (&elem, &next)| acc + if elem == next { elem } else { 0 })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::year_2017_day_01::year_2017_day_01::sum_of_repeated_characters;
+
+    #[test]
+    fn examples() {
+        assert_eq!(3, sum_of_repeated_characters("1122"));
+        assert_eq!(4, sum_of_repeated_characters("1111"));
+        assert_eq!(0, sum_of_repeated_characters("1234"));
+        assert_eq!(9, sum_of_repeated_characters("91212129"));
     }
 }
