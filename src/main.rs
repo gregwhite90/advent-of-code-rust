9cf832f57c07fa18d4e3dffc762905012d4fdffa
Gregory White <gregwhite90@gmail.com>
2023-09-27 15:28:01 -0400

    feat: add 2017-01 part two solution and rename part one function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use crate::year_2017::day_01::part_one::part_one;
+use crate::year_2017::day_01::{part_one::part_one, part_two::part_two};
 
 mod year_2017;
 
@@ -31,5 +31,6 @@ fn main() {
     let _args = Args::parse();
     // TODO: run the correct function based on parameters
 
-    println!("{}", part_one::sum_of_repeated_characters(include_str!("year_2017/day_01/input/input.txt")));
+    println!("Part one: {}", part_one::sum_of_repeated_digits(include_str!("year_2017/day_01/input/input.txt")));
+    println!("Part two: {}", part_two::sum_of_matching_halfway_around_digits(include_str!("year_2017/day_01/input/input.txt")));
 }
--- src/year_2017/day_01/part_one.rs
+++ src/year_2017/day_01/part_one.rs
@@ -2,7 +2,7 @@ pub mod part_one {
     pub use unicode_segmentation::UnicodeSegmentation;
     pub use itertools::Itertools;
     
-    pub fn sum_of_repeated_characters(text: &str) -> u32 {
+    pub fn sum_of_repeated_digits(text: &str) -> u32 {
         let graphemes: Vec<u32> = text
             .graphemes(true)
             .map(|digit| digit.parse::<u32>().unwrap())
@@ -15,13 +15,13 @@ pub mod part_one {
 
 #[cfg(test)]
 mod tests {
-    use crate::year_2017::day_01::part_one::part_one::sum_of_repeated_characters;
+    use crate::year_2017::day_01::part_one::part_one::sum_of_repeated_digits;
 
     #[test]
     fn examples() {
-        assert_eq!(3, sum_of_repeated_characters(include_str!("input/test_examples/example_1.txt")));
-        assert_eq!(4, sum_of_repeated_characters(include_str!("input/test_examples/example_2.txt")));
-        assert_eq!(0, sum_of_repeated_characters(include_str!("input/test_examples/example_3.txt")));
-        assert_eq!(9, sum_of_repeated_characters(include_str!("input/test_examples/example_4.txt")));
+        assert_eq!(3, sum_of_repeated_digits(include_str!("input/test_examples/example_1.txt")));
+        assert_eq!(4, sum_of_repeated_digits(include_str!("input/test_examples/example_2.txt")));
+        assert_eq!(0, sum_of_repeated_digits(include_str!("input/test_examples/example_3.txt")));
+        assert_eq!(9, sum_of_repeated_digits(include_str!("input/test_examples/example_4.txt")));
     }
 }
