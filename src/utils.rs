74b88b1eccd5da081c77bc4b09583d6c1e45473e
Gregory White <gregwhite90@gmail.com>
2023-10-02 11:17:30 -0400

    refactor: pull input file logic into a function

--- src/utils.rs
+++ src/utils.rs
@@ -5,4 +5,17 @@ pub mod utils {
         fn parse_input_file(&mut self, filename: &str);
         fn solve(&mut self) -> Either<i32, &str>;
     }
+
+    pub enum InputFileType {
+        Input,
+        #[allow(dead_code)] Example(u8), // constructed only in test cases
+    }
+
+    pub fn input_filename(year: u32, day: u8, input_file_type: InputFileType) -> String {
+        let file = match input_file_type {
+            InputFileType::Input => String::from("input.txt"),
+            InputFileType::Example(example_key) => format!("test_examples/example_{example_key}.txt"),
+        };
+        format!("input/year_{year}/day_{day:02}/{file}")
+    }
 }
