62cd1fc0dff68d820720631a0163ff9e9e99c9b0
Gregory White <gregwhite90@gmail.com>
2023-10-12 16:56:53 -0400

    refactor: fully share knot hashing functionality and remove duplicated code.

--- src/year_2017/day_14.rs
+++ src/year_2017/day_14.rs
@@ -6,40 +6,56 @@ const DAY: Day = crate::utils::Day { year: 2017, day: 14 };
 const ROWS: usize = 128;
 
 pub mod part_one {
-    use crate::{utils::{solution::{Solution, Answer}, io_utils}, year_2017::utils::utils::KnotHasher};
+    use crate::{utils::{solution::{Solution, Answer}, io_utils}, year_2017::utils::knot_hasher::KnotHasher};
 
     use super::ROWS;
 
-    #[derive(PartialEq, Eq, Debug)]
-    pub struct Hasher {
-        nums: Vec<u8>,
-        lengths: Vec<usize>,
-        length_idx: usize,
-        position: usize,
-        skip_size: usize,
+    #[derive(PartialEq, Eq, Debug, Default)]
+    pub struct Soln {
+        ones: u32,
     }
 
-    impl Default for Hasher {
-        fn default() -> Self {
-            Self::with_max(u8::MAX)
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+            Answer::U32(self.ones)
         }
     }
-    
-    impl KnotHasher for Hasher {
-        fn set_nums(&mut self, nums: Vec<u8>) {
-            self.nums = nums;
-        }
 
-        fn get_nums(&self) -> &Vec<u8> {
-            &self.nums
+    impl Soln {
+        fn parse_input_file(&mut self, filename: &str) {
+            let key = io_utils::file_to_string(filename);
+            self.ones = (0..ROWS)
+                .map(|idx| {
+                    let mut hasher = KnotHasher::default();
+                    hasher.parse_key(&format!("{key}-{idx}"));
+                    hasher.all_steps();
+                    u128::from_str_radix(&hasher.knot_hash(), 16)
+                        .expect("Should be able to parse base 16 string to u128.")
+                        .count_ones()
+                })
+                .sum()
         }
-        fn get_mut_nums(&mut self) -> &mut Vec<u8> {
-            &mut self.nums
-        }
+    }
+ 
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
 
-        fn set_lengths(&mut self, lengths: Vec<usize>) {
-            self.lengths = lengths;
+        #[test_case(1, Answer::U32(8108); "example_1")]
+        fn examples_are_correct(example_key: u8, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
         }
+    }    
+}
 
         fn get_lengths(&self) -> &Vec<usize> {
             &self.lengths
@@ -68,18 +84,6 @@ pub mod part_one {
         fn increment_skip_size(&mut self) {
             self.skip_size += 1;
         }
-    }
-
-    impl Hasher {
-        fn with_max(max: u8) -> Self {
-            Hasher {
-                nums: Self::range_vec_max(max),
-                lengths: vec![],
-                length_idx:0,
-                position: 0,
-                skip_size: 0,
-            }
-        }    
     }
 
     #[derive(PartialEq, Eq, Debug, Default)]
--- src/year_2017/utils.rs
+++ src/year_2017/utils.rs
@@ -1,8 +1,9 @@
-pub mod utils {
+pub mod knot_hasher {
     //! A collection of utilities used by multiple 2017 days' solutions.
     use itertools::Itertools;
     use crate::utils::io_utils;
 
+    // TODO: fix this example
     /// A trait for solutions or parts of solutions that require knot hashing
     /// (days 10 and 14).
     /// 
@@ -117,67 +118,111 @@ pub mod utils {
     ///     },
     /// );
     /// ```
-    pub trait KnotHasher {
+    
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct KnotHasher {
+        nums: Vec<u8>,
+        lengths: Vec<usize>,
+        length_idx: usize,
+        position: usize,
+        skip_size: usize,
+    }
+
+    impl Default for KnotHasher {
+        fn default() -> Self {
+            Self::with_max(u8::MAX)
+        }
+    }
+    
+    impl KnotHasher {
+
         const ROUNDS: u8 = 64;
         const DENSE_HASH_CHUNK_SIZE: usize = 16;
 
+        pub fn new(
+            nums: Vec<u8>,
+            lengths: Vec<usize>,
+            length_idx: usize,
+            position: usize,
+            skip_size: usize,
+        ) -> Self {
+            KnotHasher {
+                nums, 
+                lengths,
+                length_idx,
+                position,
+                skip_size
+            }
+        }
+
         /// Sets the private `nums` field to the specified vector.
         /// Takes ownership of the vector.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn set_nums(&mut self, nums: Vec<u8>);
+        //fn set_nums(&mut self, nums: Vec<u8>) {
+        //   self.nums = nums;
+        //}
 
         /// Gets a reference to the `nums` vector.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn get_nums(&self) -> &Vec<u8>;
-        
+        pub fn nums(&self) -> &Vec<u8> {
+            &self.nums
+        }
+
         /// Gets a mutable reference to the `nums` vector.       
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn get_mut_nums(&mut self) -> &mut Vec<u8>;
+        fn nums_mut(&mut self) -> &mut Vec<u8> {
+            &mut self.nums
+        }
 
         /// Sets the private `lengths` field to the specified vector. 
         /// Takes ownership of the vector.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn set_lengths(&mut self, lengths: Vec<usize>);
+        pub fn set_lengths(&mut self, lengths: Vec<usize>) {
+            self.lengths = lengths;
+        }
 
         /// Gets a reference to the `lengths` vector.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn get_lengths(&self) -> &Vec<usize>;
+        pub fn lengths(&self) -> &Vec<usize> {
+            &self.lengths
+        }
 
         /// Gets the next length to be used in the knot hashing algorithm.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn get_length(&self) -> usize;
+        fn length(&self) -> usize {
+            *self.lengths.get(self.length_idx).expect("Should be able to get the length at the current index.")
+        }
 
         /// Increases the index into `lengths` by one.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn increment_length_idx(&mut self);
+        fn increment_length_idx(&mut self) {
+            self.length_idx = (self.length_idx + 1) % self.lengths.len();
+        }
 
         /// Gets the current `position` into the `nums` vector.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn get_position(&self) -> usize;
+        fn position(&self) -> usize {
+            self.position
+        }
 
         /// Sets the current `position` into the `nums` vector.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn set_position(&mut self, position: usize);
+        fn set_position(&mut self, position: usize) {
+            self.position = position;
+        }
 
         /// Gets the current `skip_size` (used to increase the position after a round of
         /// the knot hashing algorithm).
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn get_skip_size(&self) -> usize;
+        fn skip_size(&self) -> usize {
+            self.skip_size
+        }
 
         /// Increases skip size by one.
-        /// No default implementation because implementation requires
-        /// access to fields of `struct` implementing the trait.
-        fn increment_skip_size(&mut self);
+        fn increment_skip_size(&mut self) {
+            self.skip_size += 1;
+        }
+
+        /// Initializes a `KnotHasher` with `nums` initially set to the range `0..=max`.
+        pub fn with_max(max: u8) -> Self {
+            KnotHasher {
+                nums: Self::range_vec_max(max),
+                lengths: vec![],
+                length_idx:0,
+                position: 0,
+                skip_size: 0,
+            }
+        }    
 
         /// Returns a vector from 0 to the specified `max`, inclusive. Used for the numbers 
         /// that will be manipulated and then knot hashed.
@@ -186,13 +231,13 @@ pub mod utils {
         }
         
         /// Parses an input file containing a single key.
-        fn parse_input_file(&mut self, filename: &str) {
+        pub fn parse_input_file(&mut self, filename: &str) {
             self.parse_key(&io_utils::file_to_string(filename));
         }
 
         /// Parses a key string to the lengths needed for the knot
         /// hashing algorithm.
-        fn parse_key(&mut self, key: &str) {
+        pub fn parse_key(&mut self, key: &str) {
             let mut lengths: Vec<usize> = key
                 .chars()
                 .map(|ch| ch as usize)
@@ -204,40 +249,40 @@ pub mod utils {
         /// Executes one step of the knot hashing algorithm: (1) reverses the order of a
         /// length of elements, (2) moves the next starting position forward, and 
         /// (3) increments the skip size.
-        fn step(&mut self) {
-            let position = self.get_position();
-            let nums_len = self.get_nums().len();
-            let length = self.get_length();
+        pub fn step(&mut self) {
+            let position = self.position();
+            let nums_len = self.nums().len();
+            let length = self.length();
             if position + length < nums_len {
-                self.get_mut_nums()[position..position + length].reverse();
+                self.nums_mut()[position..position + length].reverse();
             } else {
                 // Circularity applies and requires a special case
-                let mut full_reversal_region: Vec<u8> = self.get_nums()[position..nums_len]
+                let mut full_reversal_region: Vec<u8> = self.nums()[position..nums_len]
                     .to_vec();
                 full_reversal_region.append(
-                        &mut self.get_nums()[0..(position + length - nums_len)].to_vec()
+                        &mut self.nums()[0..(position + length - nums_len)].to_vec()
                     );
                 full_reversal_region.reverse();
-                self.get_mut_nums().splice(position..nums_len, full_reversal_region[..nums_len - position].to_vec());
-                self.get_mut_nums().splice(0..position + length - nums_len, full_reversal_region[nums_len - position..].to_vec());
+                self.nums_mut().splice(position..nums_len, full_reversal_region[..nums_len - position].to_vec());
+                self.nums_mut().splice(0..position + length - nums_len, full_reversal_region[nums_len - position..].to_vec());
             }
-            self.set_position((position + length + self.get_skip_size()) % nums_len);
+            self.set_position((position + length + self.skip_size()) % nums_len);
             self.increment_skip_size();
             self.increment_length_idx();
         }   
 
         /// Fully executes all rounds in the knot hashing algorithm
-        fn all_steps(&mut self) {
+        pub fn all_steps(&mut self) {
             for _round in 0..Self::ROUNDS {
-                for _length_idx in 0..self.get_lengths().len() {
+                for _length_idx in 0..self.lengths().len() {
                     self.step();
                 }
             }
         } 
 
         /// Returns the knot hash. Call after all the required steps have been executed.
-        fn knot_hash(&self) -> String {
-            let nums: [u8; 256] = self.get_nums().clone().try_into().expect("Should be exactly 256 numbers.");
+        pub fn knot_hash(&self) -> String {
+            let nums: [u8; 256] = self.nums().clone().try_into().expect("Should be exactly 256 numbers.");
             nums
                 .into_iter()
                 .chunks(Self::DENSE_HASH_CHUNK_SIZE)
@@ -251,91 +296,19 @@ pub mod utils {
         }
     }
 
-    // TODO: add other tests
+    // TODO: fix tests. add other tests
     #[cfg(test)]
     mod tests {
         use super::*;
 
-        #[derive(PartialEq, Eq, Debug)]
-        pub struct KnotHasherSoln {
-            nums: Vec<u8>,
-            lengths: Vec<usize>,
-            length_idx: usize,
-            position: usize,
-            skip_size: usize,
-        }
-    
-        impl Default for KnotHasherSoln {
-            fn default() -> Self {
-                Self::with_max(u8::MAX)
-            }
-        }
-        
-        impl KnotHasher for KnotHasherSoln {
-            fn set_nums(&mut self, nums: Vec<u8>) {
-                self.nums = nums;
-            }
-    
-            fn get_nums(&self) -> &Vec<u8> {
-                &self.nums
-            }
-            fn get_mut_nums(&mut self) -> &mut Vec<u8> {
-                &mut self.nums
-            }
-    
-            fn set_lengths(&mut self, lengths: Vec<usize>) {
-                self.lengths = lengths;
-            }
-    
-            fn get_lengths(&self) -> &Vec<usize> {
-                &self.lengths
-            }
-    
-            fn get_length(&self) -> usize {
-                *self.lengths.get(self.length_idx).expect("Should be able to get the length at the current index.")
-            }
-    
-            fn increment_length_idx(&mut self) {
-                self.length_idx = (self.length_idx + 1) % self.lengths.len();
-            }
-    
-            fn get_position(&self) -> usize {
-                self.position
-            }
-    
-            fn set_position(&mut self, position: usize) {
-                self.position = position;
-            }
-    
-            fn get_skip_size(&self) -> usize {
-                self.skip_size
-            }
-    
-            fn increment_skip_size(&mut self) {
-                self.skip_size += 1;
-            }
-        }
-    
-        impl KnotHasherSoln {
-            fn with_max(max: u8) -> Self {
-                KnotHasherSoln {
-                    nums: Self::range_vec_max(max),
-                    lengths: vec![],
-                    length_idx:0,
-                    position: 0,
-                    skip_size: 0,
-                }
-            }    
-        }
-    
         #[test]
         fn step_is_correct() {
-            let mut soln = KnotHasherSoln::with_max(4);
+            let mut soln = KnotHasher::with_max(4);
             soln.set_lengths(vec![0, 1, 5, 4]);
             soln.step();
             assert_eq!(
                 soln,
-                KnotHasherSoln {
+                KnotHasher {
                     nums: vec![0, 1, 2, 3, 4],
                     lengths: vec![0, 1, 5, 4],
                     length_idx: 1,
@@ -346,7 +319,7 @@ pub mod utils {
             soln.step();
             assert_eq!(
                 soln,
-                KnotHasherSoln {
+                KnotHasher {
                     nums: vec![0, 1, 2, 3, 4],
                     lengths: vec![0, 1, 5, 4],
                     length_idx: 2,
@@ -357,7 +330,7 @@ pub mod utils {
             soln.step();
             assert_eq!(
                 soln,
-                KnotHasherSoln {
+                KnotHasher {
                     nums: vec![3, 2, 1, 0, 4],
                     lengths: vec![0, 1, 5, 4],
                     length_idx: 3,
@@ -368,7 +341,7 @@ pub mod utils {
             soln.step();
             assert_eq!(
                 soln,
-                KnotHasherSoln {
+                KnotHasher {
                     nums: vec![2, 3, 4, 0, 1],
                     lengths: vec![0, 1, 5, 4],
                     length_idx: 0,
