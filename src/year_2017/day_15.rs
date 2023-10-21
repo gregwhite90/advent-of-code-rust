704c82d67f736ba9ef96a98958b8e0d7986f9d13
Gregory White <gregwhite90@gmail.com>
2023-10-21 14:20:19 -0400

    refactor: share 2017-15 functionality

--- src/year_2017/day_15.rs
+++ src/year_2017/day_15.rs
@@ -4,6 +4,8 @@ use crate::utils::Day;
 const DAY: Day = crate::utils::Day { year: 2017, day: 15 };
 
 mod utils {
+    use std::{sync::mpsc, thread};
+
     use regex::Regex;
 
     use crate::utils::io_utils;
@@ -54,6 +56,7 @@ mod utils {
 
     pub trait Year2017Day15Solution {
         fn push_generator(&mut self, generator: Generator);
+        fn pop_generator(&mut self) -> Option<Generator>;
     }
 
     pub fn parse_input_file<T>(soln: &mut T, filename: &str, part: Part)
@@ -67,6 +70,49 @@ mod utils {
             let value: u64 = captures.name("value").unwrap().as_str().parse().unwrap();
             soln.push_generator(Generator::new(i, value, part));
         });
+    }
+
+    pub fn matches<T>(soln: &mut T, iterations: usize) -> u32
+    where
+        T: Year2017Day15Solution
+    {
+        let mut matches: u32 = 0;
+
+        let (tx_a, rx_a) = mpsc::channel();
+        let (tx_b, rx_b) = mpsc::channel();
+
+        let mut gen_b = soln.pop_generator().unwrap();
+        let mut gen_a = soln.pop_generator().unwrap();
+
+        let a = thread::spawn(move || {
+            for _ in 0..iterations {
+                gen_a.step();
+                while gen_a.value_lowest_16_bits() % gen_a.multiple_of() != 0 {
+                    gen_a.step();
+                }
+                tx_a.send(gen_a.value_lowest_16_bits()).unwrap();
+            }                
+        });
+
+        let b = thread::spawn(move || {
+            for _ in 0..iterations {
+                gen_b.step();
+                while gen_b.value_lowest_16_bits() % gen_b.multiple_of() != 0 {
+                    gen_b.step();
+                }
+                tx_b.send(gen_b.value_lowest_16_bits()).unwrap();
+            }                
+        });
+
+        for _ in 0..iterations {
+            if rx_a.recv().unwrap() == rx_b.recv().unwrap() {
+                matches += 1;
+            }
+        }     
+        a.join().unwrap();
+        b.join().unwrap();
+
+        matches
     }
 }
 
@@ -85,20 +131,16 @@ pub mod part_one {
         fn push_generator(&mut self, generator: Generator) {
             self.generators.push(generator);
         }
+        
+        fn pop_generator(&mut self) -> Option<Generator> {
+            self.generators.pop()
+        }
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
             utils::parse_input_file(self, filename, Part::One);
-            let mut matches: u32 = 0;
-            for _ in 0..ITERATIONS {
-                self.generators[0].step();
-                self.generators[1].step();
-                if self.generators[0].value_lowest_16_bits() == self.generators[1].value_lowest_16_bits() {
-                    matches += 1;
-                }
-            }        
-            Answer::U32(matches)
+            Answer::U32(utils::matches(self, ITERATIONS))
         }
     }
 
@@ -122,8 +164,6 @@ pub mod part_one {
 }
 
 pub mod part_two {
-    use std::{sync::mpsc, thread};
-
     use crate::utils::solution::{Solution, Answer};
     use super::utils::{Year2017Day15Solution, Generator, Part, self};
 
@@ -138,48 +178,16 @@ pub mod part_two {
         fn push_generator(&mut self, generator: Generator) {
             self.generators.push(generator);
         }
+
+        fn pop_generator(&mut self) -> Option<Generator> {
+            self.generators.pop()
+        }
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
             utils::parse_input_file(self, filename, Part::Two);
-            let mut matches: u32 = 0;
-
-            let (tx_a, rx_a) = mpsc::channel();
-            let (tx_b, rx_b) = mpsc::channel();
-
-            let mut gen_b = self.generators.pop().unwrap();
-            let mut gen_a = self.generators.pop().unwrap();
-
-            let a = thread::spawn(move || {
-                for _ in 0..ITERATIONS {
-                    gen_a.step();
-                    while gen_a.value_lowest_16_bits() % gen_a.multiple_of() != 0 {
-                        gen_a.step();
-                    }
-                    tx_a.send(gen_a.value_lowest_16_bits()).unwrap();
-                }                
-            });
-
-            let b = thread::spawn(move || {
-                for _ in 0..ITERATIONS {
-                    gen_b.step();
-                    while gen_b.value_lowest_16_bits() % gen_b.multiple_of() != 0 {
-                        gen_b.step();
-                    }
-                    tx_b.send(gen_b.value_lowest_16_bits()).unwrap();
-                }                
-            });
-
-            for _ in 0..ITERATIONS {
-                if rx_a.recv().unwrap() == rx_b.recv().unwrap() {
-                    matches += 1;
-                }
-            }     
-            a.join().unwrap();
-            b.join().unwrap();
-
-            Answer::U32(matches)
+            Answer::U32(utils::matches(self, ITERATIONS))
         }
     }
 
