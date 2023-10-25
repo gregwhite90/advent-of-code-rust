60d0e9d9dcef7316b4880c48bd9910ac1edc6856
Gregory White <gregwhite90@gmail.com>
2023-10-25 15:12:26 -0400

    feat: solve 2017-17 part two

--- src/router.rs
+++ src/router.rs
@@ -160,7 +160,7 @@ fn get_solns(day: &Day) -> DailySolutions {
             Day { year: 2017, day: 17 },
             DailySolutions { 
                 part_one: Some(Box::new(year_2017::day_17::part_one::Soln::default())),
-                part_two: None,
+                part_two: Some(Box::new(year_2017::day_17::part_two::Soln::default())),
             },
         ),
     ]);
