f1e2616b8123706ebf2102992bf085e098a16c2a
Gregory White <gregwhite90@gmail.com>
2023-11-14 21:36:25 -0500

    docs: update docs for recent solutions

---
+++ docs/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"advent_of_code_rust":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"advent_of_code_rust/year_2017/day_16/part_two/enum.Instruction.html\" title=\"enum advent_of_code_rust::year_2017::day_16::part_two::Instruction\">Instruction</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
