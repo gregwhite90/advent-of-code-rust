3499950b049472ef7546e1b78050776d92a94436
Gregory White <gregwhite90@gmail.com>
2023-12-10 17:50:52 -0500

    feat: solve 2023-09 part two

--- src/year_2023/day_09.rs
+++ src/year_2023/day_09.rs
@@ -61,3 +61,62 @@ pub mod part_one {
         }
     }    
 }
+
+pub mod part_two {
+    use itertools::Itertools;
+
+    use crate::utils::{solution::{Solution, Answer}, io_utils};
+
+    #[derive(Debug, PartialEq, Eq, Default)]
+    pub struct Soln {
+        sum_of_extrapolated_values: i32,
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+            Answer::I32(self.sum_of_extrapolated_values)
+        }
+    }
+
+    impl Soln {
+        fn parse_input_file(&mut self, filename: &str) {
+            self.sum_of_extrapolated_values = io_utils::file_to_lines(filename)
+                .map(|line| {
+                    let history: Vec<i32> = line.split(" ")
+                        .map(|val| val.parse().unwrap())
+                        .collect();
+                    next_value(&history)
+                })
+                .sum();
+        }
+    }
+
+    fn next_value(history: &Vec<i32>) -> i32 {
+        if history.iter().all(|e| *e == 0) { return 0; }
+        let next_history: Vec<i32> = history.iter().tuple_windows()
+            .map(|(l, r)| {
+                r - l
+            })
+            .collect();
+        history[0] - next_value(&next_history)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
+
+        #[test_case(1, Answer::I32(2); "example_1")]
+        fn examples_are_correct(example_key: u8, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
+        }
+    }    
+}
