544f54452de93f0f5d815c24580d268523b739c3
Gregory White <gregwhite90@gmail.com>
2023-12-11 21:05:33 -0500

    feat: solve 2023-11 part two

--- src/year_2023/day_11.rs
+++ src/year_2023/day_11.rs
@@ -33,6 +33,7 @@ mod utils {
         fn empty_rows(&self) -> &HashSet<usize>;
         fn distances(&self) -> Ref<HashMap<(Point, Point), usize>>;
         fn distances_mut(&self) -> RefMut<HashMap<(Point, Point), usize>>;
+        fn expansion_factor(&self) -> usize;
 
         fn parse_input_file(&mut self, filename: &str) {
             let mut row: usize = 0;
@@ -66,20 +67,20 @@ mod utils {
             );
         }
 
-        /// Recalculates the sum of the minimum distances from scratch
-        fn sum_of_min_distances(&mut self, expansion_factor: usize) -> usize {
+        /// Recalculates from scratch each call
+        fn sum_of_min_distances(&mut self) -> usize {
             for (id, galaxy) in self.galaxies().iter().enumerate() {
                 for galaxy_inner in self.galaxies()[id + 1..self.galaxies().len()].iter() {
                     self.distances_mut().insert(
                         (*galaxy, *galaxy_inner), 
-                        self.distance(galaxy, galaxy_inner, expansion_factor),
+                        self.distance(galaxy, galaxy_inner),
                     );
                 }
             }
             self.distances().values().sum()
         }
 
-        fn distance(&self, l: &Point, r: &Point, expansion_factor: usize) -> usize {
+        fn distance(&self, l: &Point, r: &Point) -> usize {
             let expanded_rows: usize = match l.row.cmp(&r.row) {
                 Ordering::Equal => 0,
                 Ordering::Less => {
@@ -106,7 +107,7 @@ mod utils {
                         .count()
                 },
             };
-            l.manhattan_distance(r) + (expanded_rows + expanded_cols) * expansion_factor
+            l.manhattan_distance(r) + (expanded_rows + expanded_cols) * (self.expansion_factor() - 1)
         }
     }
 }
@@ -118,13 +119,20 @@ pub mod part_one {
 
     use super::utils::{Year2023Day11Solution, Point};
 
-    #[derive(Debug, PartialEq, Eq, Default)]
+    #[derive(Debug, PartialEq, Eq)]
     pub struct Soln {
         empty_cols: HashSet<usize>,
         empty_rows: HashSet<usize>,
         galaxies: Vec<Point>,
         distances: RefCell<HashMap<(Point, Point), usize>>,
         sum_of_min_distances: Option<usize>,
+        expansion_factor: usize,
+    }
+
+    impl Default for Soln {
+        fn default() -> Self {
+            Self::with_expansion_factor(2)
+        }
     }
 
     impl Year2023Day11Solution for Soln {
@@ -159,12 +167,29 @@ pub mod part_one {
         fn distances_mut(&self) -> RefMut<HashMap<(Point, Point), usize>> {
             self.distances.borrow_mut()
         }        
+
+        fn expansion_factor(&self) -> usize {
+            self.expansion_factor
+        }
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
             self.parse_input_file(filename);
-            Answer::U32(self.sum_of_min_distances(1) as u32)
+            Answer::U32(self.sum_of_min_distances().try_into().unwrap())
+        }
+    }
+
+    impl Soln {
+        fn with_expansion_factor(expansion_factor: usize) -> Self {
+            Self {
+                empty_cols: HashSet::new(),
+                empty_rows: HashSet::new(),
+                galaxies: vec![],
+                distances: RefCell::new(HashMap::new()),
+                sum_of_min_distances: None,
+                expansion_factor,    
+            }    
         }
     }
 
@@ -179,6 +204,108 @@ pub mod part_one {
         fn examples_are_correct(example_key: u8, answer: Answer) {
             test_utils::check_example_case(
                 &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
+        }
+    }    
+}
+
+pub mod part_two {
+    use std::{collections::{HashSet, HashMap}, cell::{RefCell, Ref, RefMut}};
+
+    use crate::utils::solution::{Solution, Answer};
+
+    use super::utils::{Year2023Day11Solution, Point};
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct Soln {
+        empty_cols: HashSet<usize>,
+        empty_rows: HashSet<usize>,
+        galaxies: Vec<Point>,
+        distances: RefCell<HashMap<(Point, Point), usize>>,
+        sum_of_min_distances: Option<usize>,
+        expansion_factor: usize,
+    }
+
+    impl Default for Soln {
+        fn default() -> Self {
+            Self::with_expansion_factor(1_000_000)
+        }
+    }
+
+    impl Year2023Day11Solution for Soln {
+        fn add_empty_row(&mut self, row: usize) {
+            self.empty_rows.insert(row);
+        }
+
+        fn add_galaxy(&mut self, galaxy: Point) {
+            self.galaxies.push(galaxy);
+        }
+
+        fn galaxies(&self) -> &Vec<Point> {
+            &self.galaxies
+        }
+
+        fn set_empty_cols(&mut self, empty_cols: HashSet<usize>) {
+            self.empty_cols = empty_cols;
+        }
+
+        fn empty_cols(&self) -> &HashSet<usize> {
+            &self.empty_cols
+        }
+
+        fn empty_rows(&self) -> &HashSet<usize> {
+            &self.empty_rows
+        }
+
+        fn distances(&self) -> Ref<HashMap<(Point, Point), usize>> {
+            self.distances.borrow()
+        }
+
+        fn distances_mut(&self) -> RefMut<HashMap<(Point, Point), usize>> {
+            self.distances.borrow_mut()
+        }     
+
+        fn expansion_factor(&self) -> usize {
+            self.expansion_factor
+        }   
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+            Answer::U64(self.sum_of_min_distances().try_into().unwrap())
+        }
+    }
+
+    impl Soln {
+        fn with_expansion_factor(expansion_factor: usize) -> Self {
+            Self {
+                empty_cols: HashSet::new(),
+                empty_rows: HashSet::new(),
+                galaxies: vec![],
+                distances: RefCell::new(HashMap::new()),
+                sum_of_min_distances: None,
+                expansion_factor,    
+            }    
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
+
+        #[test_case(1, 2, Answer::U64(374); "example_1_1")]
+        #[test_case(1, 10, Answer::U64(1_030); "example_1_10")]
+        #[test_case(1, 100, Answer::U64(8_410); "example_1_100")]
+        fn examples_are_correct(example_key: u8, expansion_factor: usize, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::with_expansion_factor(expansion_factor),
                 example_key,
                 answer,
                 &DAY,
