410cef1ac3f94445412658cd8cda0ae104888144
Gregory White <gregwhite90@gmail.com>
2024-03-28 16:17:21 -0400

    refactor: share 2023-05 functionality

--- src/year_2023/day_05.rs
+++ src/year_2023/day_05.rs
@@ -4,6 +4,10 @@ use crate::utils::Day;
 const DAY: Day = crate::utils::Day { year: 2023, day: 5 };
 
 mod utils {
+    use std::{cmp::Reverse, collections::{BTreeSet, BinaryHeap, HashMap, VecDeque}};
+
+    use regex::Regex;
+
     #[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy)]
     pub enum Category {
         Seed,
@@ -62,7 +66,6 @@ mod utils {
         pub category: Category,
         pub range: Range,
     }
-
 
     #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
     pub struct MapEntry {
@@ -170,6 +173,114 @@ mod utils {
             }
         }
     }
+
+    #[derive(Debug, PartialEq, Eq, Clone)]
+    pub struct Map {
+        source_category: Category,
+        dest_category: Category,
+        map_entries: BTreeSet<MapEntry>,
+    }
+
+    impl Map {
+        pub fn with_categories(source_category: Category, dest_category: Category) -> Self {
+            Self {
+                source_category,
+                dest_category,
+                map_entries: BTreeSet::new(),
+            }
+        }
+
+        pub fn source_category(&self) -> Category {
+            self.source_category
+        }
+
+        pub fn dest_category(&self) -> Category {
+            self.dest_category
+        }
+
+        pub fn add_map_entry(&mut self, map_entry: MapEntry) {
+            self.map_entries.insert(map_entry);
+        }
+
+        pub fn dest(&self, source: u64) -> u64 {
+            for map_entry in &self.map_entries {
+                if let Some(dst) = map_entry.dest(source) {
+                    return dst;
+                }
+            }
+            source
+        }
+
+        pub fn all_dest_ranges(&self, range: CategoryRange) -> BinaryHeap<Reverse<CategoryRange>> {
+            let mut processed = BinaryHeap::new();
+            let mut to_process = VecDeque::from([range]);
+            for map_entry in self.map_entries.iter() {
+                for _ in 0..to_process.len() {
+                    let category_range = to_process.pop_front().unwrap();
+                    let overlaps = map_entry.overlap(category_range, self.dest_category);
+                    overlaps.into_iter().for_each(|cr| {
+                        if cr.category == self.dest_category {
+                            processed.push(Reverse(cr));
+                        } else {
+                            to_process.push_back(cr);
+                        }
+                    })
+                }
+            }
+            for cr in to_process.into_iter() {
+                processed.push(Reverse(CategoryRange {
+                    category: self.dest_category,
+                    range: cr.range,
+                }));    
+            }
+            processed
+        }
+    }
+
+    /// Returns a mapping of source category to the map
+    pub fn parse_maps(lines: impl Iterator<Item = String>) -> HashMap<Category, Map> {
+        let mut maps = HashMap::new();
+        let seeds_re = Regex::new(r"seeds: (?<seeds>[ \d]+)").unwrap();
+        let map_start_re = Regex::new(r"(?<source>[a-z]+)\-to\-(?<dest>[a-z]+) map:").unwrap();
+        let map_entry_re = Regex::new(r"(?<dest_range_start>\d+) (?<source_range_start>\d+) (?<range_len>\d+)").unwrap();
+        let mut cur_map: Option<Map> = None;
+        lines.for_each(|line| {
+            if seeds_re.is_match(&line) {
+                panic!("Should not still have the seeds line in the iterator.");
+            } else if map_start_re.is_match(&line) {
+                let captures = map_start_re.captures(&line).unwrap();
+                let source_category = Category::from_str(
+                    captures.name("source").unwrap().as_str()
+                );
+                let dest_category = Category::from_str(
+                    captures.name("dest").unwrap().as_str()
+                );
+                cur_map = Some(Map::with_categories(source_category, dest_category));
+            } else if line.len() == 0 {
+                if cur_map != None {
+                    let c_m = cur_map.clone().unwrap();
+                    maps.insert(c_m.source_category(), c_m);
+                    cur_map = None;
+                }
+            } else {
+                let captures = map_entry_re.captures(&line).unwrap();
+                let dest_range_start = captures.name("dest_range_start").unwrap().as_str().parse().unwrap();
+                let source_range_start = captures.name("source_range_start").unwrap().as_str().parse().unwrap();
+                let range_len = captures.name("range_len").unwrap().as_str().parse().unwrap();
+                cur_map.as_mut().unwrap().add_map_entry(
+                    MapEntry::new(
+                        dest_range_start,
+                        source_range_start,
+                        range_len,
+                    )
+                )
+            }
+        });
+        let c_m = cur_map.unwrap();
+        maps.insert(c_m.source_category(), c_m);
+        maps
+    }
+
 
     #[cfg(test)]
     mod tests {
@@ -296,41 +407,7 @@ pub mod part_one {
 
     use crate::utils::{solution::{Solution, Answer}, io_utils};
 
-    use super::utils::{Category, MapEntry};
-
-    #[derive(Debug, PartialEq, Eq, Clone)]
-    struct Map {
-        source_category: Category,
-        dest_category: Category,
-        map_entries: Vec<MapEntry>,
-    }
-
-    impl Map {
-        fn with_categories(source_category: Category, dest_category: Category) -> Self {
-            Self {
-                source_category,
-                dest_category,
-                map_entries: vec![],
-            }
-        }
-
-        fn source_category(&self) -> Category {
-            self.source_category
-        }
-
-        fn add_map_entry(&mut self, map_entry: MapEntry) {
-            self.map_entries.push(map_entry);
-        }
-
-        fn dest(&self, source: u64) -> u64 {
-            for map_entry in &self.map_entries {
-                if let Some(dst) = map_entry.dest(source) {
-                    return dst;
-                }
-            }
-            source
-        }
-    }
+    use super::utils::{self, Category, Map};
 
     #[derive(Debug, PartialEq, Eq, Default)]
     pub struct Soln {
@@ -348,51 +425,17 @@ pub mod part_one {
     impl Soln {
         fn parse_input_file(&mut self, filename: &str) {
             let seeds_re = Regex::new(r"seeds: (?<seeds>[ \d]+)").unwrap();
-            let map_start_re = Regex::new(r"(?<source>[a-z]+)\-to\-(?<dest>[a-z]+) map:").unwrap();
-            let map_entry_re = Regex::new(r"(?<dest_range_start>\d+) (?<source_range_start>\d+) (?<range_len>\d+)").unwrap();
-            let mut cur_map: Option<Map> = None;
-            io_utils::file_to_lines(filename)
-                .for_each(|line| {
-                    if seeds_re.is_match(&line) {
-                        self.seeds = seeds_re.captures(&line)
-                            .unwrap()
-                            .name("seeds")
-                            .unwrap()
-                            .as_str()
-                            .split(" ")
-                            .map(|part| part.parse().unwrap())
-                            .collect();
-                    } else if map_start_re.is_match(&line) {
-                        let captures = map_start_re.captures(&line).unwrap();
-                        let source_category = Category::from_str(
-                            captures.name("source").unwrap().as_str()
-                        );
-                        let dest_category = Category::from_str(
-                            captures.name("dest").unwrap().as_str()
-                        );
-                        cur_map = Some(Map::with_categories(source_category, dest_category));
-                    } else if line.len() == 0 {
-                        if cur_map != None {
-                            let c_m = cur_map.clone().unwrap();
-                            self.maps.insert(c_m.source_category(), c_m);
-                            cur_map = None;
-                        }
-                    } else {
-                        let captures = map_entry_re.captures(&line).unwrap();
-                        let dest_range_start = captures.name("dest_range_start").unwrap().as_str().parse().unwrap();
-                        let source_range_start = captures.name("source_range_start").unwrap().as_str().parse().unwrap();
-                        let range_len = captures.name("range_len").unwrap().as_str().parse().unwrap();
-                        cur_map.as_mut().unwrap().add_map_entry(
-                            MapEntry::new(
-                                dest_range_start,
-                                source_range_start,
-                                range_len,
-                            )
-                        )
-                    }
-                });
-            let c_m = cur_map.unwrap();
-            self.maps.insert(c_m.source_category(), c_m);
+            let mut lines = io_utils::file_to_lines(filename);
+            let line = lines.next().unwrap();
+            self.seeds = seeds_re.captures(&line)
+                .unwrap()
+                .name("seeds")
+                .unwrap()
+                .as_str()
+                .split(" ")
+                .map(|part| part.parse().unwrap())
+                .collect();
+            self.maps = utils::parse_maps(lines);
         }
 
         fn seed_to_location(&self, seed: u64) -> u64 {
@@ -401,7 +444,7 @@ pub mod part_one {
             while next_source_category != Category::Location {
                 let map = self.maps.get(&next_source_category).unwrap();
                 next_source = map.dest(next_source);
-                next_source_category = map.dest_category;
+                next_source_category = map.dest_category();
             }
             next_source
         }
@@ -435,64 +478,13 @@ pub mod part_one {
 
 /// This implementation assumes that map entries do not overlap.
 pub mod part_two {
-    use std::{cmp::Reverse, collections::{BTreeSet, BinaryHeap, HashMap, VecDeque}};
+    use std::{cmp::Reverse, collections::{BinaryHeap, HashMap}};
     use itertools::Itertools;
     use regex::Regex;
 
     use crate::utils::{solution::{Solution, Answer}, io_utils};
 
-    use super::utils::{Category, MapEntry, CategoryRange, Range};
-
-    // TODO: pull into utils?
-    #[derive(Debug, PartialEq, Eq, Clone)]
-    struct Map {
-        source_category: Category,
-        dest_category: Category,
-        map_entries: BTreeSet<MapEntry>,
-    }
-
-    impl Map {
-        fn with_categories(source_category: Category, dest_category: Category) -> Self {
-            Self {
-                source_category,
-                dest_category,
-                map_entries: BTreeSet::new(),
-            }
-        }
-
-        fn source_category(&self) -> Category {
-            self.source_category
-        }
-
-        fn add_map_entry(&mut self, map_entry: MapEntry) {
-            self.map_entries.insert(map_entry);
-        }
-
-        fn all_dest_ranges(&self, range: CategoryRange) -> BinaryHeap<Reverse<CategoryRange>> {
-            let mut processed = BinaryHeap::new();
-            let mut to_process = VecDeque::from([range]);
-            for map_entry in self.map_entries.iter() {
-                for _ in 0..to_process.len() {
-                    let category_range = to_process.pop_front().unwrap();
-                    let overlaps = map_entry.overlap(category_range, self.dest_category);
-                    overlaps.into_iter().for_each(|cr| {
-                        if cr.category == self.dest_category {
-                            processed.push(Reverse(cr));
-                        } else {
-                            to_process.push_back(cr);
-                        }
-                    })
-                }
-            }
-            for cr in to_process.into_iter() {
-                processed.push(Reverse(CategoryRange {
-                    category: self.dest_category,
-                    range: cr.range,
-                }));    
-            }
-            processed
-        }
-    }
+    use super::utils::{self, Category, CategoryRange, Map, Range};
 
     #[derive(Debug, Default)]
     pub struct Soln {
@@ -513,61 +505,27 @@ pub mod part_two {
     impl Soln {
         fn parse_input_file(&mut self, filename: &str) {
             let seeds_re = Regex::new(r"seeds: (?<seeds>[ \d]+)").unwrap();
-            let map_start_re = Regex::new(r"(?<source>[a-z]+)\-to\-(?<dest>[a-z]+) map:").unwrap();
-            let map_entry_re = Regex::new(r"(?<dest_range_start>\d+) (?<source_range_start>\d+) (?<range_len>\d+)").unwrap();
-            let mut cur_map: Option<Map> = None;
-            io_utils::file_to_lines(filename)
-                .for_each(|line| {
-                    if seeds_re.is_match(&line) {
-                        seeds_re.captures(&line)
-                            .unwrap()
-                            .name("seeds")
-                            .unwrap()
-                            .as_str()
-                            .split(" ")
-                            .map(|part| part.parse::<u64>().unwrap())
-                            .chunks(2)
-                            .into_iter()
-                            .for_each(|mut chunk| {
-                                self.ranges.push(Reverse(CategoryRange {
-                                    category: Category::Seed, 
-                                    range: Range {
-                                        start: chunk.next().unwrap(),
-                                        len: chunk.next().unwrap(),
-                                    },
-                                }));
-                            });
-                    } else if map_start_re.is_match(&line) {
-                        let captures = map_start_re.captures(&line).unwrap();
-                        let source_category = Category::from_str(
-                            captures.name("source").unwrap().as_str()
-                        );
-                        let dest_category = Category::from_str(
-                            captures.name("dest").unwrap().as_str()
-                        );
-                        cur_map = Some(Map::with_categories(source_category, dest_category));
-                    } else if line.len() == 0 {
-                        if cur_map != None {
-                            let c_m = cur_map.clone().unwrap();
-                            self.maps.insert(c_m.source_category(), c_m);
-                            cur_map = None;
-                        }
-                    } else {
-                        let captures = map_entry_re.captures(&line).unwrap();
-                        let dest_range_start = captures.name("dest_range_start").unwrap().as_str().parse().unwrap();
-                        let source_range_start = captures.name("source_range_start").unwrap().as_str().parse().unwrap();
-                        let range_len = captures.name("range_len").unwrap().as_str().parse().unwrap();
-                        cur_map.as_mut().unwrap().add_map_entry(
-                            MapEntry::new(
-                                dest_range_start,
-                                source_range_start,
-                                range_len,
-                            )
-                        )
-                    }
+            let mut lines = io_utils::file_to_lines(filename);
+            let line = lines.next().unwrap();
+            seeds_re.captures(&line)
+                .unwrap()
+                .name("seeds")
+                .unwrap()
+                .as_str()
+                .split(" ")
+                .map(|part| part.parse::<u64>().unwrap())
+                .chunks(2)
+                .into_iter()
+                .for_each(|mut chunk| {
+                    self.ranges.push(Reverse(CategoryRange {
+                        category: Category::Seed, 
+                        range: Range {
+                            start: chunk.next().unwrap(),
+                            len: chunk.next().unwrap(),
+                        },
+                    }));
                 });
-            let c_m = cur_map.unwrap();
-            self.maps.insert(c_m.source_category(), c_m);
+            self.maps = utils::parse_maps(lines);
         }
 
         fn minimum_location_number(&mut self) -> u64 {
