06eca71fe67b36f73e0f2fbf769724cfe7e66d66
Gregory White <gregwhite90@gmail.com>
2024-04-09 12:32:30 -0400

    feat: parse 2023-22 input files

--- src/year_2023/day_22.rs
+++ src/year_2023/day_22.rs
@@ -5,17 +5,68 @@ const DAY: Day = crate::utils::Day { year: 2023, day: 22 };
 
 pub mod part_one {
 
-    use crate::utils::solution::{Answer, Solution};
+    use std::collections::HashMap;
+
+    use regex::Regex;
+
+    use crate::utils::{io_utils, solution::{Answer, Solution}};
+
+    struct Range {
+        min: u32,
+        max: u32,
+    }
+
+    struct Brick {
+        id: usize,
+        x: Range,
+        y: Range,
+        z: Range,
+        supporting_ids: Vec<usize>,
+        supported_by_ids: Vec<usize>,
+    }
+
+    impl Brick {
+        fn from_str(input: &str, id: usize, re: &Regex) -> Self {
+            let captures = re.captures(input).unwrap();
+            Self {
+                id,
+                x: Range { 
+                    min: captures.name("min_x").unwrap().as_str().parse().unwrap(), 
+                    max: captures.name("max_x").unwrap().as_str().parse().unwrap(),
+                },
+                y: Range { 
+                    min: captures.name("min_y").unwrap().as_str().parse().unwrap(), 
+                    max: captures.name("max_y").unwrap().as_str().parse().unwrap(),
+                },
+                z: Range { 
+                    min: captures.name("min_z").unwrap().as_str().parse().unwrap(), 
+                    max: captures.name("max_z").unwrap().as_str().parse().unwrap(),
+                },
+                supporting_ids: vec![],
+                supported_by_ids: vec![],
+            }
+        }
+    }
 
     #[derive(Default)]
-    pub struct Soln {}
+    pub struct Soln {
+        bricks: HashMap<usize, Brick>,
+    }
 
     impl Solution for Soln {
-        fn solve(&mut self, filename: &str) -> Answer {}
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+        }
     }
 
     impl Soln {
-        fn parse_input_file(&mut self, filename: &str) {}
+        fn parse_input_file(&mut self, filename: &str) {
+            let re = Regex::new(r"(?<min_x>\d+),(?<min_y>\d+),(?<min_z>\d+)\~(?<max_x>\d+),(?<max_y>\d+),(?<max_z>\d+)").unwrap();
+            self.bricks = io_utils::file_to_lines(filename)
+                .enumerate()
+                .map(|(id, line)| (id, Brick::from_str(&line, id, &re)))
+                .collect();
+        }
     }
 
     #[cfg(test)]
