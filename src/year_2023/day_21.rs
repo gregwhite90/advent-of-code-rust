62c5845fcad547dcc929503f9156851995f0804e
Gregory White <gregwhite90@gmail.com>
2024-04-09 11:54:18 -0400

    feat: solve 2023-21 part one test case

--- src/year_2023/day_21.rs
+++ src/year_2023/day_21.rs
@@ -5,9 +5,7 @@ const DAY: Day = crate::utils::Day { year: 2023, day: 21 };
 
 pub mod part_one {
 
-    use std::collections::HashSet;
-
-    use regex::Regex;
+    use std::collections::{HashSet, VecDeque};
 
     use crate::utils::{io_utils, solution::{Answer, Solution}};
 
@@ -30,7 +28,27 @@ pub mod part_one {
     }
 
     impl Solution for Soln {
-        fn solve(&mut self, filename: &str) -> Answer {}
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+            let mut explored: HashSet<Point> = HashSet::new();
+            let mut queue: VecDeque<Point> = VecDeque::from([self.start]);
+            // Assumes an even number of steps
+            for _ in 0..(self.steps / 2) {
+                let mut new_queue = VecDeque::new();
+                while !queue.is_empty() {
+                    let pt = queue.pop_front().unwrap();
+                    if !explored.contains(&pt) {
+                        new_queue.extend(self.two_steps_away(&pt));
+                        explored.insert(pt);
+                    }
+                }
+                queue = new_queue;
+            }
+            for pt in queue.drain(0..) {
+                explored.insert(pt);
+            }
+            Answer::Usize(explored.len())
+        }
     }
 
     impl Soln {
@@ -57,6 +75,52 @@ pub mod part_one {
                     row += 1;
                 });
         }
+
+        fn two_steps_away(&self, pt: &Point) -> VecDeque<Point> {
+            // 8 directions to check.
+            // TODO: DRY
+            let mut result = VecDeque::new();
+            if !self.rocks.contains(&Point { row: pt.row, col: pt.col - 2 })
+                && !self.rocks.contains(&Point { row: pt.row, col: pt.col - 1 }) {
+                    result.push_back(Point { row: pt.row, col: pt.col - 2 });
+            }
+            if !self.rocks.contains(&Point { row: pt.row, col: pt.col + 2 })
+                && !self.rocks.contains(&Point { row: pt.row, col: pt.col + 1 }) {
+                    result.push_back(Point { row: pt.row, col: pt.col + 2 });
+            }
+            if !self.rocks.contains(&Point { row: pt.row - 2, col: pt.col })
+                && !self.rocks.contains(&Point { row: pt.row - 1, col: pt.col }) {
+                    result.push_back(Point { row: pt.row - 2, col: pt.col });
+            }
+            if !self.rocks.contains(&Point { row: pt.row + 2, col: pt.col })
+                && !self.rocks.contains(&Point { row: pt.row + 1, col: pt.col }) {
+                    result.push_back(Point { row: pt.row + 2, col: pt.col });
+            }
+            if !self.rocks.contains(&Point { row: pt.row + 1, col: pt.col + 1 })
+                && (!self.rocks.contains(&Point { row: pt.row + 1, col: pt.col })
+                    || !self.rocks.contains(&Point { row: pt.row, col: pt.col + 1 })) {
+                        result.push_back(Point { row: pt.row + 1, col: pt.col + 1});
+            }
+            if !self.rocks.contains(&Point { row: pt.row + 1, col: pt.col - 1 })
+                && (!self.rocks.contains(&Point { row: pt.row + 1, col: pt.col })
+                    || !self.rocks.contains(&Point { row: pt.row, col: pt.col - 1 })) {
+                        result.push_back(Point { row: pt.row + 1, col: pt.col - 1});
+            }
+            if !self.rocks.contains(&Point { row: pt.row - 1, col: pt.col - 1 })
+                && (!self.rocks.contains(&Point { row: pt.row - 1, col: pt.col })
+                    || !self.rocks.contains(&Point { row: pt.row, col: pt.col - 1 })) {
+                        result.push_back(Point { row: pt.row - 1, col: pt.col - 1});
+            }
+            if !self.rocks.contains(&Point { row: pt.row - 1, col: pt.col + 1 })
+                && (!self.rocks.contains(&Point { row: pt.row - 1, col: pt.col })
+                    || !self.rocks.contains(&Point { row: pt.row, col: pt.col + 1 })) {
+                        result.push_back(Point { row: pt.row - 1, col: pt.col + 1});
+            }
+            if !result.is_empty() {
+                result.push_back(pt.clone());
+            }
+            result
+        }
     }
 
     #[cfg(test)]
@@ -66,7 +130,7 @@ pub mod part_one {
         use super::*;
         use super::super::DAY;
 
-        #[test_case(1, Answer::U32(16); "example_1")]
+        #[test_case(1, Answer::Usize(16); "example_1")]
         fn examples_are_correct(example_key: u8, answer: Answer) {
             test_utils::check_example_case(
                 &mut Soln::with_steps(6),
