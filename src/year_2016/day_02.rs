a8db61ec509f6547e866283c99a0f7f876343cb5
Gregory White <gregwhite90@gmail.com>
2024-05-20 21:20:33 -0400

    feat: solve 2016-02 parth two

--- src/year_2016/day_02.rs
+++ src/year_2016/day_02.rs
@@ -93,3 +93,115 @@ pub mod part_one {
         }
     }    
 }
+
+pub mod part_two {
+    use crate::utils::{io_utils, solution::{Answer, Solution}};
+
+    fn row_length(row: i32) -> u32 {
+        let row_abs: u32 = row.abs().try_into().unwrap();
+        5 - 2 * row_abs   
+    }
+
+    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+    enum Direction {
+        U,
+        R,
+        D,
+        L,
+    }
+
+    impl Direction {
+        fn from_char(input: char) -> Self {
+            match input {
+                'U' => Self::U,
+                'R' => Self::R,
+                'D' => Self::D,
+                'L' => Self::L,
+                _ => panic!("Unrecognized Direction character."),
+            }
+        }
+    }
+
+    #[derive(Debug, Default, PartialEq, Eq, Hash, Clone, Copy)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    impl Point {
+        fn to_digit(&self) -> u32 {
+            let previous_row_lengths: u32 = (-2..self.y).map(|row| row_length(row)).sum();
+            let cur_row_length = row_length(self.y);
+            let cur_row_idx: u32 = ((cur_row_length as i32 - 1) / 2 + self.x).try_into().unwrap();
+            previous_row_lengths + cur_row_idx + 1
+        }
+
+        fn to_char(&self) -> char {
+            char::from_digit(self.to_digit(), 16).unwrap().to_ascii_uppercase()
+        }
+
+        fn manhattan_distance(&self) -> i32 {
+            self.x.abs() + self.y.abs()
+        }
+
+        fn step(&self, max_manhattan_distance: i32, direction: Direction) -> Point {
+            let next_position = match direction {
+                Direction::U => Point { x: self.x, y: self.y - 1 },
+                Direction::L => Point { x: self.x - 1, y: self.y },
+                Direction::D => Point { x: self.x, y: self.y + 1 },
+                Direction::R => Point { x: self.x + 1, y: self.y },
+            };
+            if next_position.manhattan_distance() > max_manhattan_distance {
+                self.clone()
+            }
+            else {
+                next_position
+            }
+        }
+    }
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        code: String,
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+            Answer::String(self.code.clone())
+        }
+    }
+
+    impl Soln {
+        fn parse_input_file(&mut self, filename: &str) {
+            // TODO: make dynamic?
+            let max_manhattan_distance = 2;
+            let mut position = Point { x: -2, y: 0 };
+            io_utils::file_to_lines(filename)
+                .for_each(|line| {
+                    line.chars().for_each(|ch| {
+                        position = position.step(max_manhattan_distance, Direction::from_char(ch));
+                    });
+                    self.code.push(position.to_char());
+                });
+        }   
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
+
+        #[test_case(1, Answer::String(String::from("5DB3")); "example_1")]
+        fn examples_are_correct(example_key: u8, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
+        }
+    }    
+}
