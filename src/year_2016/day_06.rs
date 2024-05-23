531bfb5fa3c1ca8b7d5954750b92eaaa23d916fe
Gregory White <gregwhite90@gmail.com>
2024-05-22 21:50:47 -0400

    refactor: use a more flexible form for 2016-06 part two

--- src/year_2016/day_06.rs
+++ src/year_2016/day_06.rs
@@ -68,38 +68,51 @@ pub mod part_two {
 
     use crate::utils::{io_utils, solution::{Answer, Solution}};
 
-    #[derive(Debug, Default)]
+    fn least_frequent_char(char_frequency: &HashMap<char, u32>) -> char {
+        *char_frequency.iter().min_by_key(|(_, count)| *count).unwrap().0
+    }
+
     pub struct Soln {
-        most_frequent_chars: Vec<HashMap<char, u32>>,
+        char_frequencies: Vec<HashMap<char, u32>>,
+        frequency_to_char: Box<dyn Fn(&HashMap<char, u32>) -> char>
+    }
+
+    impl Default for Soln {
+        fn default() -> Self {
+            Self {
+                char_frequencies: Vec::new(),
+                frequency_to_char: Box::new(least_frequent_char),
+            }
+        }
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
             self.parse_input_file(filename);
-            Answer::String(self.most_frequent_string())
+            Answer::String(self.frequencies_to_string())
         }
     }
 
     impl Soln {
         fn parse_input_file(&mut self, filename: &str) {
             for line in io_utils::file_to_lines(filename) {
-                if self.most_frequent_chars.is_empty() {
+                if self.char_frequencies.is_empty() {
                     for ch in line.chars() {
-                        self.most_frequent_chars.push(HashMap::from([(ch, 1)]));
+                        self.char_frequencies.push(HashMap::from([(ch, 1)]));
                     }
                 } else {
                     for (idx, ch) in line.char_indices() {
-                        *self.most_frequent_chars[idx].entry(ch).or_insert(0) += 1;
+                        *self.char_frequencies[idx].entry(ch).or_insert(0) += 1;
                     }
                 }
             }
         }   
 
-        fn most_frequent_string(&self) -> String {
-            assert!(!self.most_frequent_chars.is_empty());
-            self.most_frequent_chars.iter().map(|mfc| {
-                mfc.iter().min_by_key(|(_, count)| *count).unwrap().0
-            }).collect::<String>()
+        fn frequencies_to_string(&self) -> String {
+            assert!(!self.char_frequencies.is_empty());
+            self.char_frequencies.iter()
+                .map(&self.frequency_to_char)
+                .collect::<String>()
         }
     }
 
