eba56cc90cce3360997b3d4869f6e7d292df4112
Gregory White <gregwhite90@gmail.com>
2024-05-22 21:57:08 -0400

    refactor: update 2016-06 part two to use shared functionality

--- src/year_2016/day_06.rs
+++ src/year_2016/day_06.rs
@@ -3,6 +3,49 @@ use crate::utils::Day;
 #[cfg(test)]
 const DAY: Day = crate::utils::Day { year: 2016, day: 6 };
 
+mod utils {
+    use std::collections::HashMap;
+
+    use crate::utils::io_utils;
+
+    pub struct RepetitionDecoder {
+        char_frequencies: Vec<HashMap<char, u32>>,
+        frequency_to_char: Box<dyn Fn(&HashMap<char, u32>) -> char>
+    }
+
+    impl RepetitionDecoder {
+        pub fn new(frequency_to_char: Box<dyn Fn(&HashMap<char, u32>) -> char>) -> Self {
+            Self {
+                char_frequencies: Vec::new(),
+                frequency_to_char: Box::new(frequency_to_char),
+            }
+        }
+    }
+
+    impl RepetitionDecoder {
+        pub fn parse_input_file(&mut self, filename: &str) {
+            for line in io_utils::file_to_lines(filename) {
+                if self.char_frequencies.is_empty() {
+                    for ch in line.chars() {
+                        self.char_frequencies.push(HashMap::from([(ch, 1)]));
+                    }
+                } else {
+                    for (idx, ch) in line.char_indices() {
+                        *self.char_frequencies[idx].entry(ch).or_insert(0) += 1;
+                    }
+                }
+            }
+        }   
+
+        pub fn frequencies_to_string(&self) -> String {
+            assert!(!self.char_frequencies.is_empty());
+            self.char_frequencies.iter()
+                .map(&self.frequency_to_char)
+                .collect::<String>()
+        }
+    }
+}
+
 pub mod part_one {
     use std::collections::HashMap;
 
@@ -62,57 +105,33 @@ pub mod part_one {
     }    
 }
 
-
 pub mod part_two {
     use std::collections::HashMap;
 
-    use crate::utils::{io_utils, solution::{Answer, Solution}};
+    use crate::utils::solution::{Answer, Solution};
+
+    use super::utils::RepetitionDecoder;
 
     fn least_frequent_char(char_frequency: &HashMap<char, u32>) -> char {
         *char_frequency.iter().min_by_key(|(_, count)| *count).unwrap().0
     }
 
     pub struct Soln {
-        char_frequencies: Vec<HashMap<char, u32>>,
-        frequency_to_char: Box<dyn Fn(&HashMap<char, u32>) -> char>
+        repetition_decoder: RepetitionDecoder,
     }
 
     impl Default for Soln {
         fn default() -> Self {
             Self {
-                char_frequencies: Vec::new(),
-                frequency_to_char: Box::new(least_frequent_char),
+                repetition_decoder: RepetitionDecoder::new(Box::new(least_frequent_char)),
             }
         }
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
-            self.parse_input_file(filename);
-            Answer::String(self.frequencies_to_string())
-        }
-    }
-
-    impl Soln {
-        fn parse_input_file(&mut self, filename: &str) {
-            for line in io_utils::file_to_lines(filename) {
-                if self.char_frequencies.is_empty() {
-                    for ch in line.chars() {
-                        self.char_frequencies.push(HashMap::from([(ch, 1)]));
-                    }
-                } else {
-                    for (idx, ch) in line.char_indices() {
-                        *self.char_frequencies[idx].entry(ch).or_insert(0) += 1;
-                    }
-                }
-            }
-        }   
-
-        fn frequencies_to_string(&self) -> String {
-            assert!(!self.char_frequencies.is_empty());
-            self.char_frequencies.iter()
-                .map(&self.frequency_to_char)
-                .collect::<String>()
+            self.repetition_decoder.parse_input_file(filename);
+            Answer::String(self.repetition_decoder.frequencies_to_string())
         }
     }
 
