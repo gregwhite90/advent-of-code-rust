aaa1ac65b70410002c26a1316212d6bbd511d694
Gregory White <gregwhite90@gmail.com>
2024-05-31 10:32:12 -0400

    refactor: share 2016-17 functionality

--- src/year_2016/day_17.rs
+++ src/year_2016/day_17.rs
@@ -3,12 +3,13 @@ use crate::utils::Day;
 #[cfg(test)]
 const DAY: Day = crate::utils::Day { year: 2016, day: 17 };
 
-pub mod part_one {
-    use std::collections::BinaryHeap;
+mod utils {
+    use std::{cmp::max, collections::BinaryHeap};
 
+    use itertools::izip;
     use md5::{Digest, Md5};
 
-    use crate::utils::{io_utils, solution::{Answer, Solution}};
+    use crate::utils::io_utils;
 
     #[derive(Debug, PartialEq, Eq)]
     struct Point {
@@ -59,69 +60,91 @@ pub mod part_one {
     }
 
     #[derive(Debug, Default)]
-    pub struct Soln {
+    pub struct VaultGrid {
         passcode: String,
     }
 
-    impl Solution for Soln {
-        fn solve(&mut self, filename: &str) -> Answer {
-            self.parse_input_file(filename);
-            Answer::String(self.shortest_path())
-        }
-    }
-
-    impl Soln {
-        fn parse_input_file(&mut self, filename: &str) {
+    impl VaultGrid {
+        pub fn parse_input_file(&mut self, filename: &str) {
             self.passcode = io_utils::file_to_string(filename);
         }
 
-        fn shortest_path(&self) -> String {
+        pub fn shortest_path(&self) -> String {
             let mut pq = BinaryHeap::from([Path::default()]);
             while !pq.is_empty() {
                 let path = pq.pop().unwrap();
-                if path.is_finished() { return path.to_string(); }
                 // TODO: could optimize to only calculate position once.
-                let position = path.position();
-                // TODO: hash it
+                if path.is_finished() { return path.to_string(); }
                 let hash = format!(
                     "{:x}",
                     Md5::digest(format!("{}{}", self.passcode, path.to_string()).as_bytes()),
                 );
-                let mut chars = hash.chars();
-                let u = char_is_open(chars.next().unwrap());
-                let d = char_is_open(chars.next().unwrap());
-                let l = char_is_open(chars.next().unwrap());
-                let r = char_is_open(chars.next().unwrap());
-                if u && position.y != 0 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('U');
-                    pq.push(new_path);
-                }
-                if l && position.x != 0 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('L');
-                    pq.push(new_path);
-                }
-                if d && position.y != 3 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('D');
-                    pq.push(new_path);
-                }
-                if r && position.x != 3 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('R');
-                    pq.push(new_path);
-                }
+                push_new_paths(hash, &path, &mut pq);
             }
             panic!("Explored all paths without reaching destination.");
         }
 
+        pub fn longest_path(&self) -> usize {
+            let mut pq = BinaryHeap::from([Path::default()]);
+            let mut longest_path: Option<usize> = None;
+            while !pq.is_empty() {
+                let path = pq.pop().unwrap();
+                // TODO: could optimize to only calculate position once.
+                if path.is_finished() { 
+                    longest_path = match longest_path {
+                        None => Some(path.len()),
+                        Some(lp) => Some(max(lp, path.len())),
+                    };
+                    continue;
+                }
+                let hash = format!(
+                    "{:x}",
+                    Md5::digest(format!("{}{}", self.passcode, path.to_string()).as_bytes()),
+                );
+                push_new_paths(hash, &path, &mut pq);
+            }
+            longest_path.unwrap()
+        }
+    }
+
+    fn push_new_paths(hash: String, path: &Path, pq: &mut BinaryHeap<Path>) {
+        let position = path.position();
+        let chars = hash.chars();
+        for (is_open, is_not_border, ch) in izip!(
+            chars.take(4).map(|ch| char_is_open(ch)),
+            [position.y != 0, position.y != 3, position.x != 0, position.x != 3],
+            ['U', 'D', 'L', 'R'],
+        ) {
+            if is_open && is_not_border {
+                let mut new_path = path.clone();
+                new_path.push_step(ch);
+                pq.push(new_path);    
+            }
+        }
     }
 
     fn char_is_open(ch: char) -> bool {
         match ch {
             'b'..='f' => true,
             _ => false,
+        }
+    }
+}
+
+pub mod part_one {
+    use crate::utils::solution::{Answer, Solution};
+
+    use super::utils::VaultGrid;
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        vault_grid: VaultGrid,
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.vault_grid.parse_input_file(filename);
+            Answer::String(self.vault_grid.shortest_path())
         }
     }
 
@@ -147,169 +170,19 @@ pub mod part_one {
 }
 
 pub mod part_two {
-    use std::{cmp::max, collections::BinaryHeap};
+    use crate::utils::solution::{Answer, Solution};
 
-    use md5::{Digest, Md5};
-
-    use crate::utils::{io_utils, solution::{Answer, Solution}};
-
-    #[derive(Debug, PartialEq, Eq)]
-    struct Point {
-        x: usize,
-        y: usize,
-    }
-
-    #[derive(Debug, Default, PartialEq, Eq, Clone)]
-    struct Path {
-        steps: Vec<char>,
-    }
-
-    impl Path {
-        fn len(&self) -> usize {
-            self.steps.len()
-        }
-
-        fn position(&self) -> Point {
-            Point {
-                x: self.steps.iter().filter(|step| **step == 'R').count() - self.steps.iter().filter(|step| **step == 'L').count(),
-                y: self.steps.iter().filter(|step| **step == 'D').count() - self.steps.iter().filter(|step| **step == 'U').count(),
-            }
-        }
-
-        fn is_finished(&self) -> bool {
-            self.position() == Point { x: 3, y: 3 }
-        }
-
-        fn to_string(&self) -> String {
-            self.steps.iter().collect::<String>()
-        }
-
-        fn push_step(&mut self, step: char) {
-            self.steps.push(step);
-        }
-    }
-
-    impl Ord for Path {
-        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-            other.len().cmp(&self.len()).then_with(|| self.steps.cmp(&other.steps))
-        }
-    }
-
-    impl PartialOrd for Path {
-        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-            Some(self.cmp(other))
-        }
-    }
+    use super::utils::VaultGrid;
 
     #[derive(Debug, Default)]
     pub struct Soln {
-        passcode: String,
+        vault_grid: VaultGrid,
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
-            self.parse_input_file(filename);
-            Answer::Usize(self.longest_path())
-        }
-    }
-
-    impl Soln {
-        fn parse_input_file(&mut self, filename: &str) {
-            self.passcode = io_utils::file_to_string(filename);
-        }
-
-        fn shortest_path(&self) -> String {
-            let mut pq = BinaryHeap::from([Path::default()]);
-            while !pq.is_empty() {
-                let path = pq.pop().unwrap();
-                if path.is_finished() { return path.to_string(); }
-                // TODO: could optimize to only calculate position once.
-                let position = path.position();
-                let hash = format!(
-                    "{:x}",
-                    Md5::digest(format!("{}{}", self.passcode, path.to_string()).as_bytes()),
-                );
-                let mut chars = hash.chars();
-                let u = char_is_open(chars.next().unwrap());
-                let d = char_is_open(chars.next().unwrap());
-                let l = char_is_open(chars.next().unwrap());
-                let r = char_is_open(chars.next().unwrap());
-                if u && position.y != 0 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('U');
-                    pq.push(new_path);
-                }
-                if l && position.x != 0 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('L');
-                    pq.push(new_path);
-                }
-                if d && position.y != 3 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('D');
-                    pq.push(new_path);
-                }
-                if r && position.x != 3 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('R');
-                    pq.push(new_path);
-                }
-            }
-            panic!("Explored all paths without reaching destination.");
-        }
-
-        fn longest_path(&self) -> usize {
-            let mut pq = BinaryHeap::from([Path::default()]);
-            let mut longest_path: Option<usize> = None;
-            while !pq.is_empty() {
-                let path = pq.pop().unwrap();
-                if path.is_finished() { 
-                    longest_path = match longest_path {
-                        None => Some(path.len()),
-                        Some(lp) => Some(max(lp, path.len())),
-                    };
-                    continue;
-                }
-                // TODO: could optimize to only calculate position once.
-                let position = path.position();
-                let hash = format!(
-                    "{:x}",
-                    Md5::digest(format!("{}{}", self.passcode, path.to_string()).as_bytes()),
-                );
-                let mut chars = hash.chars();
-                let u = char_is_open(chars.next().unwrap());
-                let d = char_is_open(chars.next().unwrap());
-                let l = char_is_open(chars.next().unwrap());
-                let r = char_is_open(chars.next().unwrap());
-                if u && position.y != 0 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('U');
-                    pq.push(new_path);
-                }
-                if l && position.x != 0 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('L');
-                    pq.push(new_path);
-                }
-                if d && position.y != 3 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('D');
-                    pq.push(new_path);
-                }
-                if r && position.x != 3 {
-                    let mut new_path = path.clone();
-                    new_path.push_step('R');
-                    pq.push(new_path);
-                }
-            }
-            longest_path.unwrap()
-        }
-    }
-
-    fn char_is_open(ch: char) -> bool {
-        match ch {
-            'b'..='f' => true,
-            _ => false,
+            self.vault_grid.parse_input_file(filename);
+            Answer::Usize(self.vault_grid.longest_path())
         }
     }
 
