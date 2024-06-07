ba02e5794b7eca09315e3e60459a6943c1905d42
Gregory White <gregwhite90@gmail.com>
2024-06-07 16:17:41 -0400

    feat: parse file to new type of grid for 2016-22 part two

--- src/year_2016/day_22.rs
+++ src/year_2016/day_22.rs
@@ -12,30 +12,41 @@ mod utils {
     use crate::utils::io_utils;
 
     lazy_static! {
-        static ref NODE_RE: Regex = Regex::new(r"/dev/grid/node\-x(?<x>\d+)\-y(?<y>\d+)\s+(?<size>\d+)T\s+(?<used>\d+)T\s+(?<avail>\d+)T\s+(?<use_pct>\d+)\%").unwrap();
+        pub static ref NODE_RE: Regex = Regex::new(r"/dev/grid/node\-x(?<x>\d+)\-y(?<y>\d+)\s+(?<size>\d+)T\s+(?<used>\d+)T\s+(?<avail>\d+)T\s+(?<use_pct>\d+)\%").unwrap();
     }
 
-    #[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy)]
-    struct Position {
-        x: usize,
-        y: usize,
+    #[derive(Debug, Default, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy)]
+    pub struct Position {
+        pub x: usize,
+        pub y: usize,
     }
 
     impl Position {
-        fn new(x: usize, y: usize) -> Self {
+        pub fn new(x: usize, y: usize) -> Self {
             Self { x, y }
         }
+
+        pub fn adjacent_positions(&self) -> Vec<Position> {
+            let mut adjacent = Vec::new();
+            if self.x != 0 { adjacent.push(Position::new(self.x - 1, self.y)); }
+            if self.y != 0 { adjacent.push(Position::new(self.x, self.y - 1)); }
+            for new_pos in [Position::new(self.x, self.y + 1), Position::new(self.x + 1, self.y)] {
+                adjacent.push(new_pos);
+            }
+            adjacent
+        }    
+    
     }
 
     #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
-    struct Node {
-        position: Position,
-        used: u32,
-        avail: u32,
+    pub struct Node {
+        pub position: Position,
+        pub used: u32,
+        pub avail: u32,
     }
 
     impl Node {
-        fn from_str(input: &str) -> Self {
+        pub fn from_str(input: &str) -> Self {
             let captures = NODE_RE.captures(input).unwrap();
             let x = captures.name("x").unwrap().as_str().parse().unwrap();
             let y = captures.name("y").unwrap().as_str().parse().unwrap();
@@ -45,8 +56,16 @@ mod utils {
             Self { position, used, avail }
         }
 
+        pub fn position(&self) -> Position {
+            self.position
+        }
+
         fn fits_self(&self) -> bool {
             self.used > 0 && self.avail >= self.used
+        }
+
+        pub fn size(&self) -> u32 {
+            self.avail + self.used
         }
 
         fn fits(&self, other: &Self) -> bool {
@@ -104,114 +123,6 @@ mod utils {
                     .sum::<usize>()
             }).sum::<usize>() - self.nodes_fitting_self()
         }
-
-        pub fn fewest_steps(&self) -> usize {
-            // TODO: Avoid generating all the legal moves every time.
-            let mut visited: HashSet<GridPathStatus> = HashSet::new();
-            let goal_data = self.nodes.keys()
-                .filter(|pos| pos.y == 0)
-                .max_by_key(|pos| pos.x)
-                .unwrap();
-            let mut pq = BinaryHeap::from([Reverse(GridPath::new(
-                0, 
-                *goal_data,
-                self.nodes.clone(), 
-            ))]);
-            while !pq.is_empty() {
-                let path = pq.pop().unwrap().0;
-                if path.is_finished() { return path.moves; }
-                let status = path.to_status();
-                if visited.contains(&status) { continue; }
-                visited.insert(status);
-                pq.append(&mut path.next_paths());
-            }
-            panic!("Explored all states without finishing.");
-        }
-    }
-
-    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
-    struct GridPath {
-        moves: usize,
-        goal_data: Position,
-        nodes: BTreeMap<Position, Node>,
-    }
-
-    impl GridPath {
-        fn new(moves: usize, goal_data: Position, nodes: BTreeMap<Position, Node>) -> Self {
-            Self {
-                moves,
-                goal_data,
-                nodes,
-            }
-        }
-
-        fn next_paths(&self) -> BinaryHeap<Reverse<Self>> {
-            self.legal_moves().iter().map(|m| {
-                let mut nodes = self.nodes.clone();
-                let amt = nodes.get_mut(&m.src).unwrap().move_from();
-                nodes.get_mut(&m.dst).unwrap().move_to(amt);
-                let goal_data = if self.goal_data == m.src {
-                    m.dst
-                } else {
-                    self.goal_data
-                };
-                Reverse(Self::new(
-                    self.moves + 1,
-                    goal_data,
-                    nodes,
-                ))
-            }).collect()
-        }
-
-        fn is_finished(&self) -> bool {
-            self.goal_data == Position::new(0, 0)
-        }
-
-        fn to_status(&self) -> GridPathStatus {
-            GridPathStatus {
-                nodes: self.nodes.clone(),
-                goal_data: self.goal_data,
-            }
-        }
-
-        fn legal_moves(&self) -> BTreeSet<Move> {
-            let mut moves = BTreeSet::new();
-            for (pos, node) in self.nodes.iter() {
-                for adj in self.adjacent_positions(pos) {
-                    if node.fits(self.nodes.get(&adj).unwrap()) {
-                        moves.insert(Move { src: adj, dst: *pos});
-                    }
-                    if self.nodes.get(&adj).unwrap().fits(node) {
-                        moves.insert(Move { src: *pos, dst: adj});
-                    }                    
-                }
-            }
-            moves
-        }
-    
-        fn adjacent_positions(&self, pos: &Position) -> Vec<Position> {
-            let mut adjacent = Vec::new();
-            if pos.x != 0 { adjacent.push(Position::new(pos.x - 1, pos.y)); }
-            if pos.y != 0 { adjacent.push(Position::new(pos.x, pos.y - 1)); }
-            for new_pos in [Position::new(pos.x, pos.y + 1), Position::new(pos.x + 1, pos.y)] {
-                if self.nodes.contains_key(&new_pos) {
-                    adjacent.push(new_pos);
-                }
-            }
-            adjacent
-        }    
-    }
-
-    #[derive(Debug, PartialEq, Eq, Hash)]
-    struct GridPathStatus {
-        nodes: BTreeMap<Position, Node>,
-        goal_data: Position,
-    }
-
-    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
-    struct Move {
-        src: Position,
-        dst: Position,
     }
 }
 
@@ -233,10 +144,68 @@ pub mod part_one {
     }
 }
 
+/// This solution is not a general purpose solution (I wrote one, but it is too slow
+/// for this specific problem). It instead relies on the pattern seen in the input data
+/// (both the example in the puzzle statement and the full input data): the grid is made
+/// up of one empty node, several useless nodes (very large and very full),
+/// and the rest interchangeable nodes (full enough that no other node's data fits, 
+/// and small enough that their data can be moved around).
 pub mod part_two {
-    use crate::utils::solution::{Answer, Solution};
+    use std::collections::{HashMap, HashSet};
 
-    use super::utils::Grid;
+    use crate::utils::{io_utils, solution::{Answer, Solution}};
+
+    use super::utils::{Position, Node};
+
+    #[derive(Debug, Default)]
+    struct Grid {
+        rows: usize,
+        cols: usize,
+        empty: Position,
+        useless: HashSet<Position>,
+    }
+
+    impl Grid {
+        fn parse_input_file(&mut self, filename: &str) {
+            let nodes: HashMap<Position, Node> = io_utils::file_to_lines(filename)
+                .skip(2)
+                .map(|line| {
+                    let node = Node::from_str(&line);
+                    (node.position(), node)
+                })
+                .collect();
+            self.rows = nodes.keys()
+                .map(|pos| pos.y)
+                .max()
+                .unwrap() + 1;
+            self.cols = nodes.keys()
+                .map(|pos| pos.x)
+                .max()
+                .unwrap() + 1;
+            
+            // Find the one empty node
+            let empties: Vec<&Node> = nodes.values().filter(|node| node.used == 0).collect();
+            assert_eq!(empties.len(), 1);
+            self.empty = empties[0].position;
+            
+            // Find the useless nodes
+            let mut nodes_less_empty = nodes.clone();
+            nodes_less_empty.remove(&self.empty);
+            self.useless = nodes_less_empty.iter().filter(|(pos, node)| {
+                nodes_less_empty.values().all(|other| node.avail < other.used)
+                    && pos.adjacent_positions().iter().all(|adj| {
+                        !nodes.contains_key(adj) || node.used > nodes.get(adj).unwrap().size()
+                    })
+            })
+                .map(|(pos, _node)| *pos)
+                .collect();
+        }
+
+        fn fewest_steps(&self) -> usize {
+            0
+        }
+    }
+
 
     #[derive(Debug, Default)]
     pub struct Soln {
