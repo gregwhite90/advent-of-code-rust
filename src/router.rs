29ee14f12e26a4b777cd714be20e9e1463be57c5
Gregory White <gregwhite90@gmail.com>
2024-06-07 11:31:59 -0400

    feat (wip): solve 2016-22 part two with general purpose solution (but too slow in this problem case)

--- src/router.rs
+++ src/router.rs
@@ -195,7 +195,7 @@ fn get_solns(day: &Day) -> DailySolutions {
             Day { year: 2016, day: 22 },
             DailySolutions { 
                 part_one: Some(Box::new(year_2016::day_22::part_one::Soln::default())),
-                part_two: None,
+                part_two: Some(Box::new(year_2016::day_22::part_two::Soln::default())),
             },
         ),
         (
--- src/year_2016/day_22.rs
+++ src/year_2016/day_22.rs
@@ -4,7 +4,7 @@ use crate::utils::Day;
 const DAY: Day = crate::utils::Day { year: 2016, day: 22 };
 
 mod utils {
-    use std::collections::HashMap;
+    use std::{cmp::Reverse, collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet}};
 
     use lazy_static::lazy_static;
     use regex::Regex;
@@ -15,7 +15,7 @@ mod utils {
         static ref NODE_RE: Regex = Regex::new(r"/dev/grid/node\-x(?<x>\d+)\-y(?<y>\d+)\s+(?<size>\d+)T\s+(?<used>\d+)T\s+(?<avail>\d+)T\s+(?<use_pct>\d+)\%").unwrap();
     }
 
-    #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+    #[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy)]
     struct Position {
         x: usize,
         y: usize,
@@ -27,13 +27,11 @@ mod utils {
         }
     }
 
-    #[derive(Debug, Clone)]
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
     struct Node {
         position: Position,
-        size: u32,
         used: u32,
         avail: u32,
-        use_pct: u32,
     }
 
     impl Node {
@@ -42,21 +40,35 @@ mod utils {
             let x = captures.name("x").unwrap().as_str().parse().unwrap();
             let y = captures.name("y").unwrap().as_str().parse().unwrap();
             let position = Position::new(x, y);
-            let size = captures.name("size").unwrap().as_str().parse().unwrap();
             let used = captures.name("used").unwrap().as_str().parse().unwrap();
             let avail = captures.name("avail").unwrap().as_str().parse().unwrap();
-            let use_pct = captures.name("use_pct").unwrap().as_str().parse().unwrap();
-            Self { position, size, used, avail, use_pct }
+            Self { position, used, avail }
         }
 
         fn fits_self(&self) -> bool {
             self.used > 0 && self.avail >= self.used
         }
+
+        fn fits(&self, other: &Self) -> bool {
+            other.used > 0 && self.avail >= other.used
+        }
+
+        fn move_from(&mut self) -> u32 {
+            let to_move = self.used;
+            self.used = 0;
+            self.avail += to_move;
+            to_move
+        }
+
+        fn move_to(&mut self, amt: u32) {
+            self.used += amt;
+            self.avail -= amt;
+        }
     }
 
     #[derive(Debug, Default)]
     pub struct Grid {
-        nodes: HashMap<Position, Node>,
+        nodes: BTreeMap<Position, Node>,
     }
 
     impl Grid {
@@ -92,6 +104,114 @@ mod utils {
                     .sum::<usize>()
             }).sum::<usize>() - self.nodes_fitting_self()
         }
+
+        pub fn fewest_steps(&self) -> usize {
+            // TODO: Avoid generating all the legal moves every time.
+            let mut visited: HashSet<GridPathStatus> = HashSet::new();
+            let goal_data = self.nodes.keys()
+                .filter(|pos| pos.y == 0)
+                .max_by_key(|pos| pos.x)
+                .unwrap();
+            let mut pq = BinaryHeap::from([Reverse(GridPath::new(
+                0, 
+                *goal_data,
+                self.nodes.clone(), 
+            ))]);
+            while !pq.is_empty() {
+                let path = pq.pop().unwrap().0;
+                if path.is_finished() { return path.moves; }
+                let status = path.to_status();
+                if visited.contains(&status) { continue; }
+                visited.insert(status);
+                pq.append(&mut path.next_paths());
+            }
+            panic!("Explored all states without finishing.");
+        }
+    }
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+    struct GridPath {
+        moves: usize,
+        goal_data: Position,
+        nodes: BTreeMap<Position, Node>,
+    }
+
+    impl GridPath {
+        fn new(moves: usize, goal_data: Position, nodes: BTreeMap<Position, Node>) -> Self {
+            Self {
+                moves,
+                goal_data,
+                nodes,
+            }
+        }
+
+        fn next_paths(&self) -> BinaryHeap<Reverse<Self>> {
+            self.legal_moves().iter().map(|m| {
+                let mut nodes = self.nodes.clone();
+                let amt = nodes.get_mut(&m.src).unwrap().move_from();
+                nodes.get_mut(&m.dst).unwrap().move_to(amt);
+                let goal_data = if self.goal_data == m.src {
+                    m.dst
+                } else {
+                    self.goal_data
+                };
+                Reverse(Self::new(
+                    self.moves + 1,
+                    goal_data,
+                    nodes,
+                ))
+            }).collect()
+        }
+
+        fn is_finished(&self) -> bool {
+            self.goal_data == Position::new(0, 0)
+        }
+
+        fn to_status(&self) -> GridPathStatus {
+            GridPathStatus {
+                nodes: self.nodes.clone(),
+                goal_data: self.goal_data,
+            }
+        }
+
+        fn legal_moves(&self) -> BTreeSet<Move> {
+            let mut moves = BTreeSet::new();
+            for (pos, node) in self.nodes.iter() {
+                for adj in self.adjacent_positions(pos) {
+                    if node.fits(self.nodes.get(&adj).unwrap()) {
+                        moves.insert(Move { src: adj, dst: *pos});
+                    }
+                    if self.nodes.get(&adj).unwrap().fits(node) {
+                        moves.insert(Move { src: *pos, dst: adj});
+                    }                    
+                }
+            }
+            moves
+        }
+    
+        fn adjacent_positions(&self, pos: &Position) -> Vec<Position> {
+            let mut adjacent = Vec::new();
+            if pos.x != 0 { adjacent.push(Position::new(pos.x - 1, pos.y)); }
+            if pos.y != 0 { adjacent.push(Position::new(pos.x, pos.y - 1)); }
+            for new_pos in [Position::new(pos.x, pos.y + 1), Position::new(pos.x + 1, pos.y)] {
+                if self.nodes.contains_key(&new_pos) {
+                    adjacent.push(new_pos);
+                }
+            }
+            adjacent
+        }    
+    }
+
+    #[derive(Debug, PartialEq, Eq, Hash)]
+    struct GridPathStatus {
+        nodes: BTreeMap<Position, Node>,
+        goal_data: Position,
+    }
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+    struct Move {
+        src: Position,
+        dst: Position,
     }
 }
 
@@ -111,4 +231,40 @@ pub mod part_one {
             Answer::Usize(self.grid.viable_pairs())
         }
     }
+}
+
+pub mod part_two {
+    use crate::utils::solution::{Answer, Solution};
+
+    use super::utils::Grid;
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        grid: Grid,
+    }
+    
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.grid.parse_input_file(filename);
+            Answer::Usize(self.grid.fewest_steps())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
+
+        #[test_case(1, Answer::Usize(7); "example_1")]
+        fn examples_are_correct(example_key: u8, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
+        }
+    }    
 }
