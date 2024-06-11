3a0ca5a38c5a5c7d9fa16e06f4d124b22894ffa7
Gregory White <gregwhite90@gmail.com>
2024-06-11 15:10:47 -0400

    feat: solve 2018-02 part two

--- src/year_2018/day_02.rs
+++ src/year_2018/day_02.rs
@@ -7,7 +7,7 @@ pub mod part_one {
     use std::collections::HashMap;
 
     use crate::utils::{io_utils, solution::{Answer, Solution}};
-    
+
     #[derive(Debug)]
     pub struct BoxID {
         letter_count: HashMap<char, u32>,
@@ -61,6 +61,66 @@ pub mod part_one {
         use super::super::DAY;
 
         #[test_case(1, Answer::Usize(12); "example_1")]
+        fn examples_are_correct(example_key: u8, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
+        }
+    }    
+}
+
+pub mod part_two {
+    use crate::utils::{io_utils, solution::{Answer, Solution}};
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        ids: Vec<String>,    
+        shared_id: String,
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.parse_input_file(filename);
+            Answer::String(self.shared_id.clone())
+        }
+    }
+
+    fn different_char_indices(l: &str, r: &str) -> Vec<usize> {
+        l.chars().zip(r.chars()).enumerate().filter(|(_idx, (l_ch, r_ch))| {
+            l_ch != r_ch
+        })
+            .map(|(idx, (_, _))| idx)
+            .collect()
+    }
+
+    impl Soln {
+        fn parse_input_file(&mut self, filename: &str) {
+            for line in io_utils::file_to_lines(filename) {
+                for prev in self.ids.iter() {
+                    let diff = different_char_indices(&line, &prev);
+                    if diff.len() == 1 {
+                        let mut l = line.clone();
+                        l.remove(diff[0]);
+                        self.shared_id = l;
+                        return;
+                    }
+                }
+                self.ids.push(line);
+            }
+        }   
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
+
+        #[test_case(2, Answer::String("fgij".to_string()); "example_2")]
         fn examples_are_correct(example_key: u8, answer: Answer) {
             test_utils::check_example_case(
                 &mut Soln::default(),
