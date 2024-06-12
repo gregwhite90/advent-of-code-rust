c460d39ba9527f5d5a364627e55d88fcda3f141f
Gregory White <gregwhite90@gmail.com>
2024-06-12 16:59:15 -0400

    refactor: share 2018-03 functionality

--- src/year_2018/day_03.rs
+++ src/year_2018/day_03.rs
@@ -3,13 +3,13 @@ use crate::utils::Day;
 #[cfg(test)]
 const DAY: Day = crate::utils::Day { year: 2018, day: 3 };
 
-pub mod part_one {
-    use std::collections::HashMap;
+mod utils {
+    use std::collections::{HashMap, HashSet};
 
     use itertools::iproduct;
     use regex::Regex;
 
-    use crate::utils::{io_utils, solution::{Answer, Solution}};
+    use crate::utils::io_utils;
 
     #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
     struct Point {
@@ -18,36 +18,60 @@ pub mod part_one {
     }
 
     #[derive(Debug, Default)]
-    pub struct Soln {
-        claims: HashMap<Point, usize>,    
+    pub struct Fabric {
+        ids: HashSet<usize>,
+        claims: HashMap<Point, HashSet<usize>>,    
     }
 
-    impl Solution for Soln {
-        fn solve(&mut self, filename: &str) -> Answer {
-            self.parse_input_file(filename);
-            Answer::Usize(self.points_with_multiple_claims())
-        }
-    }
-
-    impl Soln {
-        fn parse_input_file(&mut self, filename: &str) {
+    impl Fabric {
+        pub fn parse_input_file(&mut self, filename: &str) {
             let line_re = Regex::new(r"#(?<id>\d+) \@ (?<x>\d+),(?<y>\d+)\: (?<width>\d+)x(?<height>\d+)").unwrap();
             io_utils::file_to_lines(filename)
                 .for_each(|line| {
                     let captures = line_re.captures(&line).unwrap();
+                    let id: usize = captures.name("id").unwrap().as_str().parse().unwrap();
+                    self.ids.insert(id);
                     let x: usize = captures.name("x").unwrap().as_str().parse().unwrap();
                     let y: usize = captures.name("y").unwrap().as_str().parse().unwrap();
                     let width: usize = captures.name("width").unwrap().as_str().parse().unwrap();
                     let height: usize = captures.name("height").unwrap().as_str().parse().unwrap();
                     for (col, row) in iproduct!(x..x + width, y..y + height) {
                         let point = Point {x: col, y: row };
-                        *self.claims.entry(point).or_default() += 1;
+                        self.claims.entry(point).or_default().insert(id);
                     }
                 });
         }   
 
-        fn points_with_multiple_claims(&self) -> usize {
-            self.claims.values().filter(|count| **count > 1).count()
+        pub fn points_with_multiple_claims(&self) -> usize {
+            self.claims.values().filter(|ids| ids.len() > 1).count()
+        }
+
+        pub fn id_of_nonoverlapping_claim(&self) -> usize {
+            let mut remaining = self.ids.clone();
+            for ids in self.claims.values().filter(|ids| ids.len() > 1) {
+                remaining.retain(|id| !ids.contains(id));
+                if remaining.len() == 1 { break; }
+            }
+            assert!(remaining.len() == 1);
+            remaining.into_iter().next().unwrap()
+        }
+    }
+}
+
+pub mod part_one {
+    use crate::utils::solution::{Answer, Solution};
+
+    use super::utils::Fabric;
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        fabric: Fabric,    
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.fabric.parse_input_file(filename);
+            Answer::Usize(self.fabric.points_with_multiple_claims())
         }
     }
 
@@ -71,63 +95,19 @@ pub mod part_one {
 }
 
 pub mod part_two {
-    use std::collections::{HashMap, HashSet};
+    use crate::utils::solution::{Answer, Solution};
 
-    use itertools::iproduct;
-    use regex::Regex;
-
-    use crate::utils::{io_utils, solution::{Answer, Solution}};
-
-    #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-    struct Point {
-        x: usize,
-        y: usize,
-    }
+    use super::utils::Fabric;
 
     #[derive(Debug, Default)]
     pub struct Soln {
-        ids: HashSet<usize>,
-        claims: HashMap<Point, HashSet<usize>>,    
+        fabric: Fabric,
     }
 
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
-            self.parse_input_file(filename);
-            Answer::Usize(self.id_of_nonoverlapping_claim())
-        }
-    }
-
-    impl Soln {
-        fn parse_input_file(&mut self, filename: &str) {
-            let line_re = Regex::new(r"#(?<id>\d+) \@ (?<x>\d+),(?<y>\d+)\: (?<width>\d+)x(?<height>\d+)").unwrap();
-            io_utils::file_to_lines(filename)
-                .for_each(|line| {
-                    let captures = line_re.captures(&line).unwrap();
-                    let id: usize = captures.name("id").unwrap().as_str().parse().unwrap();
-                    self.ids.insert(id);
-                    let x: usize = captures.name("x").unwrap().as_str().parse().unwrap();
-                    let y: usize = captures.name("y").unwrap().as_str().parse().unwrap();
-                    let width: usize = captures.name("width").unwrap().as_str().parse().unwrap();
-                    let height: usize = captures.name("height").unwrap().as_str().parse().unwrap();
-                    for (col, row) in iproduct!(x..x + width, y..y + height) {
-                        let point = Point {x: col, y: row };
-                        self.claims.entry(point).or_default().insert(id);
-                    }
-                });
-        }   
-
-        fn points_with_multiple_claims(&self) -> usize {
-            self.claims.values().filter(|ids| ids.len() > 1).count()
-        }
-
-        fn id_of_nonoverlapping_claim(&self) -> usize {
-            let mut remaining = self.ids.clone();
-            for ids in self.claims.values().filter(|ids| ids.len() > 1) {
-                remaining.retain(|id| !ids.contains(id));
-                if remaining.len() == 1 { break; }
-            }
-            assert!(remaining.len() == 1);
-            remaining.into_iter().next().unwrap()
+            self.fabric.parse_input_file(filename);
+            Answer::Usize(self.fabric.id_of_nonoverlapping_claim())
         }
     }
 
