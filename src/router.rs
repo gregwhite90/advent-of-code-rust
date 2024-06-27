02e2a4b67abd6bec811d2e6e9c2fae7c9b7c105f
Gregory White <gregwhite90@gmail.com>
2024-06-27 16:12:11 -0400

    feat: solve 2018-16 part two

--- src/router.rs
+++ src/router.rs
@@ -489,7 +489,7 @@ fn get_solns(day: &Day) -> DailySolutions {
             Day { year: 2018, day: 16 },
             DailySolutions { 
                 part_one: Some(Box::new(year_2018::day_16::part_one::Soln::default())),
-                part_two: None,
+                part_two: Some(Box::new(year_2018::day_16::part_two::Soln::default())),
             },
         ),
         (
--- src/year_2018/day_16.rs
+++ src/year_2018/day_16.rs
@@ -4,12 +4,12 @@ use crate::utils::Day;
 const DAY: Day = crate::utils::Day { year: 2018, day: 16 };
 
 mod utils {
-    use std::collections::HashSet;
+    use std::collections::{HashMap, HashSet};
 
     use strum::IntoEnumIterator;
     use strum_macros::EnumIter;
 
-    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, EnumIter)]
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, EnumIter)]
     pub enum Operation {
         ADDR,
         ADDI,
@@ -98,6 +98,85 @@ mod utils {
                 .collect()
         }
     }
+
+    #[derive(Debug)]
+    pub struct CPU {
+        opcodes: HashMap<usize, HashSet<Operation>>,
+        registers: Vec<usize>,
+    }
+
+    impl Default for CPU {
+        fn default() -> Self {
+            Self {
+                opcodes: (0..16).map(|opcode| {
+                    (opcode, HashSet::from_iter(Operation::iter()))
+                }).collect(),
+                registers: vec![0; 4],
+            }
+            
+        }
+    }
+
+    impl CPU {
+        // TODO: pass in another vec instead of all the individual args?
+        pub fn process_sample(
+            &mut self,             
+            before: &Vec<usize>,
+            opcode: usize,
+            a: usize,
+            b: usize,
+            c: usize,
+            after: &Vec<usize>,
+        ) {
+            let matching = Operation::matching_operations(
+                before,
+                a,
+                b,
+                c,
+                after,
+            );
+            self.opcodes.get_mut(&opcode).unwrap().retain(|op| matching.contains(op));
+        }
+
+        pub fn fully_determine_opcodes(&mut self) {
+            // Iteratively reduce the sets of possible operations for each opcode
+            // by the operations that are fully determinatively assigned to one opcode.
+            // After the first call to perform_operation, this is trivial.
+            let mut fully_determined = HashSet::new();
+            self.opcodes.values().filter(|possible| possible.len() == 1)
+                .for_each(|possible| {
+                    possible.iter().for_each(|op| { fully_determined.insert(*op); });
+                });
+            while self.opcodes.values().any(|possible| possible.len() > 1) {
+                self.opcodes.values_mut()
+                .filter(|possible| possible.len() > 1)
+                .for_each(|possible| {
+                    possible.retain(|op| !fully_determined.contains(op));
+                    if possible.len() == 1 {
+                        possible.iter().for_each(|op| { fully_determined.insert(*op); });
+                    }
+                });
+            }
+        }
+
+        pub fn perform_operation(&mut self, operation: Vec<usize>) {
+            self.fully_determine_opcodes();
+            let opcode = operation[0];
+            let possible_operations = self.opcodes.get(&opcode).unwrap();
+            assert_eq!(1, possible_operations.len());
+            let op = possible_operations.iter().next().unwrap();
+            op.execute(
+                &mut self.registers, 
+                operation[1],
+                operation[2],
+                operation[3], 
+            );
+        }
+
+        pub fn register_value(&self, register: usize) -> usize {
+            self.registers[register]
+        }
+    }
 }
 
 pub mod part_one {
@@ -179,6 +258,66 @@ pub mod part_one {
                 answer,
                 &DAY,
             );
+        }
+    }
+}
+
+pub mod part_two {
+    use regex::Regex;
+
+    use crate::utils::{io_utils, solution::{Answer, Solution}};
+
+    use super::utils::CPU;
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        cpu: CPU,
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            let mut before = None;
+            let mut operation: Option<Vec<usize>> = None;
+            let mut after = None;
+            let registers_re = Regex::new(r"(?<seq>(Before)|(After)):\s+\[(?<registers>[\d\, ]+)\]").unwrap();
+            io_utils::file_to_lines(filename).for_each(|line| {
+                if line.len() == 0 {
+                    if let Some(af) = &after {
+                        assert_ne!(before, None);
+                        // NOTE: changed from part one
+                        self.cpu.process_sample(
+                            &before.as_ref().unwrap(),
+                            operation.as_ref().unwrap()[0],
+                            operation.as_ref().unwrap()[1], 
+                            operation.as_ref().unwrap()[2], 
+                            operation.as_ref().unwrap()[3], 
+                            &af
+                        );
+                        before = None;
+                        operation = None;
+                        after = None;                        
+                    }
+                } else if let Some(captures) = registers_re.captures(&line) {
+                    let seq = captures.name("seq").unwrap().as_str();
+                    let registers = captures.name("registers").unwrap().as_str().split(", ")
+                        .map(|val| val.parse().unwrap())
+                        .collect();
+                    match seq {
+                        "Before" => before = Some(registers),
+                        "After"  => after  = Some(registers),
+                        _ => panic!("Unrecognized sequence"),
+                    }
+                } else {
+                    let op = line.split(" ").map(|val| val.parse().unwrap()).collect();
+                    if before == None {
+                        // perform operation
+                        self.cpu.perform_operation(op);
+                    } else {
+                        operation = Some(op);
+                    }
+                }
+            });
+            Answer::Usize(self.cpu.register_value(0))
         }
     }
 }
