21c784e4b7002c65c0204eb7f29ca370e896ea2f
Gregory White <gregwhite90@gmail.com>
2024-11-11 18:01:18 -0500

    feat: calculate summed-area table for ease of searching sub-grid sums

--- src/year_2018/day_11.rs
+++ src/year_2018/day_11.rs
@@ -33,6 +33,7 @@ mod utils {
     pub struct Grid {
         serial_number: usize,
         power_levels: HashMap<Point, i64>,
+        summed_area_table: HashMap<Point, i64>,
     }
 
     impl Grid {
@@ -40,6 +41,7 @@ mod utils {
             Self {
                 serial_number,
                 power_levels: HashMap::new(),
+                summed_area_table: HashMap::new(),
             }
         }
 
@@ -56,6 +58,41 @@ mod utils {
                 let power_level: i64 = power_level.try_into().unwrap();
                 power_level - 5
             })
+        }
+
+        pub fn build_summed_area_table(&mut self) {
+            for x in 1..=300 {
+                for y in 1..=300 {
+                    let pt = Point { x, y };
+                    let mut sat_entry = self.power_level(&pt);
+                    if x > 1 {
+                        sat_entry += self.summed_area_table.get(&Point { x: x - 1, y }).unwrap()
+                    }
+                    if y > 1 {
+                        sat_entry += self.summed_area_table.get(&Point { x, y: y - 1 }).unwrap()
+                    }
+                    if x > 1 && y > 1 {
+                        sat_entry -= self.summed_area_table.get(&Point { x: x - 1, y: y - 1 }).unwrap();
+                    }
+                    self.summed_area_table.insert(pt, sat_entry);
+                }
+            }
+        }
+
+        pub fn total_power(&self, point: &Point, window_size: usize) -> i64 {
+            // The summed-area table methodology does not work for single points
+            assert!(window_size > 1);
+            let mut total_power = *self.summed_area_table.get(&Point{ x: point.x + window_size - 1, y: point.y + window_size - 1}).unwrap();
+            if point.x > 1 {
+                total_power -= *self.summed_area_table.get(&Point{ x: point.x - 1, y: point.y + window_size - 1 }).unwrap();
+            }
+            if point.y > 1 {
+                total_power -= self.summed_area_table.get(&Point{ x: point.x + window_size - 1, y: point.y - 1 }).unwrap();
+            }
+            if point.x > 1 && point.y > 1 {
+                total_power += self.summed_area_table.get(&Point{ x: point.x - 1, y: point.y - 1 }).unwrap();
+            }
+            total_power
         }
     }
 
@@ -92,6 +129,14 @@ mod utils {
         fn power_levels_are_correct(x: usize, y: usize, serial_number: usize, power_level: i64) {
             let mut grid = Grid::new(serial_number);
             assert_eq!(grid.power_level(&Point { x, y }), power_level);
+        }
+
+        #[test_case(90, 269, 16, 18, 113; "example_1")]
+        #[test_case(232, 251, 12, 42, 119; "example_2")]
+        fn total_powers_are_correct(x: usize, y: usize, window_size: usize, serial_number: usize, total_power: i64) {
+            let mut grid = Grid::new(serial_number);
+            grid.build_summed_area_table();
+            assert_eq!(grid.total_power(&Point{ x, y }, window_size), total_power);
         }
     }
 }
@@ -142,7 +187,10 @@ pub mod part_one {
     }
 }
 
-
+/**
+ * Solution to part two uses a summed-area table, as first found suggested on the Reddit
+ * thread of solutions.
+ */
 pub mod part_two {
     use crate::utils::solution::{Answer, Solution};
 
