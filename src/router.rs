f063368f9a42c778331dfc50147819ab64c9ca7d
Gregory White <gregwhite90@gmail.com>
2024-11-26 14:15:08 -0500

    feat: solve 2015-15 part two

--- src/router.rs
+++ src/router.rs
@@ -111,7 +111,7 @@ fn get_solns(day: &Day) -> DailySolutions {
             Day { year: 2015, day: 15 },
             DailySolutions { 
                 part_one: Some(Box::new(year_2015::day_15::part_one::Soln::default())),
-                part_two: None,
+                part_two: Some(Box::new(year_2015::day_15::part_two::Soln::default())),
             },
         ),
         (
--- src/year_2015/day_15.rs
+++ src/year_2015/day_15.rs
@@ -14,12 +14,14 @@ mod utils {
     #[derive(Debug, Default)]
     struct Ingredients {
         features: Array2<i64>,
+        calories: Array1<i64>,
     }
 
     impl Ingredients {
         pub fn parse_input_file(&mut self, filename: &str) {
-            let ingredient_re = Regex::new(r"(?<name>\w+): capacity (?<capacity>\-?\d+), durability (?<durability>\-?\d+), flavor (?<flavor>\-?\d+), texture (?<texture>\-?\d+), calories (?<calories>\-?\d+)").unwrap();
+            let ingredient_re = Regex::new(r"(?<name>\w+): capacity (?<capacity>\-?\d+), durability (?<durability>\-?\d+), flavor (?<flavor>\-?\d+), texture (?<texture>\-?\d+), calories (?<calories>\d+)").unwrap();
             let mut num_ingredients = 0;
+            let mut cals: Vec<i64> = Vec::new();
             let ingredients = io_utils::file_to_lines(filename)
                 .map(|line| {
                     num_ingredients += 1;
@@ -28,9 +30,16 @@ mod utils {
                     let durability: i64 = caps.name("durability").unwrap().as_str().parse().unwrap();
                     let flavor: i64 = caps.name("flavor").unwrap().as_str().parse().unwrap();
                     let texture: i64 = caps.name("texture").unwrap().as_str().parse().unwrap();
+                    let calories = caps.name("calories").unwrap().as_str().parse().unwrap();
+                    cals.push(calories);
                     [capacity, durability, flavor, texture]
                 });
             self.features = Array::from_iter(ingredients.flatten()).into_shape((num_ingredients, 4)).unwrap().reversed_axes();
+            self.calories = Array::from_vec(cals);
+        }
+
+        fn calories(&self, recipe: &Array1<i64>) -> i64 {
+            self.calories.dot(recipe)
         }
 
         fn score(&self, recipe: &Array1<i64>) -> i64 {
@@ -48,14 +57,18 @@ mod utils {
             self.ingredients.parse_input_file(filename);
         }
 
-        pub fn max_total_score(&self) -> i64 {
+        pub fn max_total_score(&self, calorie_count: Option<i64>) -> i64 {
             // TODO: figure out how to do this dynamically
             let mut res = i64::MIN;
             for i in 0..=100 {
-                for j in 0..=(100 - i) {
+                  for j in 0..=(100 - i) {
                     for k in 0..=(100 - i - j) {
                         let l = 100 - i - j - k;
                         let recipe = array![i, j, k, l];
+                        if let Some(calories) = calorie_count {
+                            let cals = self.ingredients.calories(&recipe);
+                            if cals != calories { continue; }
+                        }
                         let score = self.ingredients.score(&recipe);
                         if score > res { res = score; }
                     }
@@ -79,7 +92,7 @@ pub mod part_one {
     impl Solution for Soln {
         fn solve(&mut self, filename: &str) -> Answer {
             self.recipe_optimizer.parse_input_file(filename);
-            Answer::I64(self.recipe_optimizer.max_total_score())
+            Answer::I64(self.recipe_optimizer.max_total_score(None))
         }
     }
 
@@ -91,6 +104,42 @@ pub mod part_one {
         use super::super::DAY;
 
         #[test_case(1, Answer::I64(62_842_880); "example_1")]
+        fn examples_are_correct(example_key: u8, answer: Answer) {
+            test_utils::check_example_case(
+                &mut Soln::default(),
+                example_key,
+                answer,
+                &DAY,
+            );
+        }
+    }    
+}
+
+pub mod part_two {
+    use crate::utils::solution::{Answer, Solution};
+
+    use super::utils::RecipeOptimizer;
+
+    #[derive(Debug, Default)]
+    pub struct Soln {
+        recipe_optimizer: RecipeOptimizer,
+    }
+
+    impl Solution for Soln {
+        fn solve(&mut self, filename: &str) -> Answer {
+            self.recipe_optimizer.parse_input_file(filename);
+            Answer::I64(self.recipe_optimizer.max_total_score(Some(500)))
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use test_case::test_case;
+        use crate::utils::{test_utils, solution::Answer};
+        use super::*;
+        use super::super::DAY;
+
+        #[test_case(1, Answer::I64(57_600_000); "example_1")]
         fn examples_are_correct(example_key: u8, answer: Answer) {
             test_utils::check_example_case(
                 &mut Soln::default(),
