66a00cdb4355d65e59fe25bebc707627d9211c6c
Greg White <gregory.white@overstory.com>
2025-12-02 20:43:55 -0500

    feat: solve 2025-02 part two

--- src/router.rs
+++ src/router.rs
@@ -944,7 +944,7 @@ fn get_solns(day: &Day) -> DailySolutions {
             Day { year: 2025, day: 2 },
             DailySolutions { 
                 part_one: Some(Box::new(year_2025::day_02::part_one::Soln::default())),
-                part_two: None,
+                part_two: Some(Box::new(year_2025::day_02::part_two::Soln::default())),
             },
         ),
     ]);
--- src/year_2025/day_02.rs
+++ src/year_2025/day_02.rs
@@ -4,6 +4,8 @@ use crate::utils::Day;
 const DAY: Day = crate::utils::Day { year: 2025, day: 2 };
 
 mod utils {
+    use std::collections::HashSet;
+
     #[derive(Debug)]
     pub struct Range {
         invalid_ids_sum: usize,
@@ -16,39 +18,37 @@ mod utils {
         ) -> Self {
             let bounds: Vec<&str> = input.split('-').collect();
             let bounds_digits: Vec<usize> = bounds.iter().map(|bound| bound.len()).collect();
-            let invalid_ids_sum = (bounds_digits[0]..=bounds_digits[1])
-                .map(|num_digits| {
+            let invalid_ids: HashSet<usize> = (bounds_digits[0]..=bounds_digits[1])
+                .flat_map(|num_digits| {
+                    let lower: usize = std::cmp::max(bounds[0].parse().unwrap(), 10usize.pow((num_digits - 1) as u32));
+                    let upper: usize = std::cmp::min(bounds[1].parse().unwrap(), 10usize.pow(num_digits as u32) - 1);
                     let max_repeats: usize = if more_than_two_repeats_allowed {
                         num_digits
                     } else {
                         2
                     };
                     (2..=max_repeats)
-                        .map(|repeats| {
-                            if num_digits % repeats == 0 {
-                                // If num_digits is divisible by the number of repeats, invalid IDs are possible
-                                let exp: u32 = (num_digits / 2) as u32;
-                                let lower: usize = std::cmp::max(bounds[0].parse().unwrap(), 10usize.pow((num_digits - 1) as u32));
-                                let lower_prefix = lower / 10usize.pow(exp);
-                                let upper: usize = std::cmp::min(bounds[1].parse().unwrap(), 10usize.pow(num_digits as u32) - 1);
-                                let upper_prefix = upper / 10usize.pow(exp);
-                                (lower_prefix..=upper_prefix)
-                                    .map(|prefix| {
-                                        prefix * 10usize.pow(exp) + prefix
-                                    })
-                                    .filter(|num| {
-                                        lower <= *num && *num <= upper
-                                    })
-                                    .sum()
-                            } else {
-                                // If num_digits is not divisible by the number of repeats, can't have any invalid IDs
-                                0
-                            }
+                        .filter(|repeats| num_digits % *repeats == 0)
+                        .flat_map(|repeats| {
+                            // If num_digits is divisible by the number of repeats, invalid IDs are possible
+                            let prefix_len: usize = num_digits / repeats;
+                            let lower_prefix = &lower.to_string()[..prefix_len];
+                            let upper_prefix = &upper.to_string()[..prefix_len];
+                            (lower_prefix.parse::<usize>().unwrap()..=upper_prefix.parse::<usize>().unwrap())
+                                .map(|prefix| {
+                                    prefix.to_string().repeat(repeats).parse::<usize>().unwrap()
+                                })
+                                .filter(|num| {
+                                    lower <= *num && *num <= upper
+                                })
+                                .collect::<HashSet<usize>>()
                         })
-                        .sum::<usize>()
+                        .collect::<HashSet<usize>>()
                 })
-                .sum();
-            Self { invalid_ids_sum } 
+                .collect();
+            Self { 
+                invalid_ids_sum: invalid_ids.iter().sum()
+            } 
         }
 
         pub fn invalid_ids_sum(&self) -> usize {
@@ -136,7 +136,8 @@ pub mod part_two {
                     .map(|line| {
                         line.split(',')
                             .map(|range_str| {
-                                Range::from_str(range_str, true).invalid_ids_sum()
+                                let invalid_ids_sum = Range::from_str(range_str, true).invalid_ids_sum();
+                                invalid_ids_sum
                             })
                             .sum::<usize>()
                     })
@@ -160,11 +161,16 @@ pub mod part_two {
         #[test_case("1-31", 33; "different_lengths_odd_to_even")]
         #[test_case("1-161", 606; "different_lengths_odd_to_odd")]
         #[test_case("78-101", 187; "different_lengths_even_to_odd")]
-        #[test_case("78-1320", 8_163; "different_lengths_even_to_even")]
+        #[test_case(
+            "78-1320", 
+            88 + 99 + 111 + 222 + 333 + 444 + 555 + 666 + 777 + 888 + 999 + 1010 + 1111 + 1212 + 1313;
+            "different_lengths_even_to_even"
+        )]
         #[test_case("11-22", 33; "puzzle_11-22")]
         #[test_case("95-115", 210; "puzzle_95-115")]
         #[test_case("998-1012", 2_009; "puzzle_998-1012")]
         #[test_case("1188511880-1188511890", 1_188_511_885; "puzzle_1188511880-1188511890")]
+        #[test_case("222220-222224", 222_222; "puzzle_222220-222224")]
         #[test_case("1698522-1698528", 0; "puzzle_1698522-1698528")]
         #[test_case("446443-446449", 446_446; "puzzle_446443-446449")]
         #[test_case("38593856-38593862", 38_593_859; "puzzle_38593856-38593862")]
